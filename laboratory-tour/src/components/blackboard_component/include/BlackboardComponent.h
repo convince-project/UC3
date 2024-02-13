12263832408100862f83456ef883dc0adde6c138
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-02-13 13:25:22 +0100

    added int32 to blacboard interfaces
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- laboratory-tour/src/components/blackboard_component/include/BlackboardComponent.h
+++ laboratory-tour/src/components/blackboard_component/include/BlackboardComponent.h
@@ -10,6 +10,8 @@
 #include <rclcpp/rclcpp.hpp>
 #include <blackboard_interfaces/srv/get_double_blackboard.hpp>
 #include <blackboard_interfaces/srv/set_double_blackboard.hpp>
+#include <blackboard_interfaces/srv/get_int_blackboard.hpp>
+#include <blackboard_interfaces/srv/set_int_blackboard.hpp>
 #include <blackboard_interfaces/srv/set_string_blackboard.hpp>
 #include <blackboard_interfaces/srv/get_string_blackboard.hpp>
 #include <map>
@@ -23,24 +25,32 @@ public:
 
     bool close();
     void spin();
-    void GetDouble([[maybe_unused]] const std::shared_ptr<blackboard_interfaces::srv::GetDoubleBlackboard::Request> request,
-               [[maybe_unused]] std::shared_ptr<blackboard_interfaces::srv::GetDoubleBlackboard::Response>      response);
-    void SetDouble([[maybe_unused]] const std::shared_ptr<blackboard_interfaces::srv::SetDoubleBlackboard::Request> request,
-               [[maybe_unused]] std::shared_ptr<blackboard_interfaces::srv::SetDoubleBlackboard::Response>      response);
-    void GetString([[maybe_unused]] const std::shared_ptr<blackboard_interfaces::srv::GetStringBlackboard::Request> request,
-               [[maybe_unused]] std::shared_ptr<blackboard_interfaces::srv::GetStringBlackboard::Response>      response);
-    void SetString([[maybe_unused]] const std::shared_ptr<blackboard_interfaces::srv::SetStringBlackboard::Request> request,
-               [[maybe_unused]] std::shared_ptr<blackboard_interfaces::srv::SetStringBlackboard::Response>      response);
+    void GetDouble( const std::shared_ptr<blackboard_interfaces::srv::GetDoubleBlackboard::Request> request,
+                std::shared_ptr<blackboard_interfaces::srv::GetDoubleBlackboard::Response>      response);
+    void SetDouble( const std::shared_ptr<blackboard_interfaces::srv::SetDoubleBlackboard::Request> request,
+                std::shared_ptr<blackboard_interfaces::srv::SetDoubleBlackboard::Response>      response);
+    void GetInt( const std::shared_ptr<blackboard_interfaces::srv::GetIntBlackboard::Request> request,
+                std::shared_ptr<blackboard_interfaces::srv::GetIntBlackboard::Response>      response);
+    void SetInt( const std::shared_ptr<blackboard_interfaces::srv::SetIntBlackboard::Request> request,
+                std::shared_ptr<blackboard_interfaces::srv::SetIntBlackboard::Response>      response);
+    void GetString( const std::shared_ptr<blackboard_interfaces::srv::GetStringBlackboard::Request> request,
+                std::shared_ptr<blackboard_interfaces::srv::GetStringBlackboard::Response>      response);
+    void SetString( const std::shared_ptr<blackboard_interfaces::srv::SetStringBlackboard::Request> request,
+                std::shared_ptr<blackboard_interfaces::srv::SetStringBlackboard::Response>      response);
 
 private:
     rclcpp::Node::SharedPtr m_node;
     rclcpp::Service<blackboard_interfaces::srv::SetDoubleBlackboard>::SharedPtr m_setDoubleService;
     rclcpp::Service<blackboard_interfaces::srv::GetDoubleBlackboard>::SharedPtr m_getDoubleService;
+    rclcpp::Service<blackboard_interfaces::srv::SetIntBlackboard>::SharedPtr m_setIntService;
+    rclcpp::Service<blackboard_interfaces::srv::GetIntBlackboard>::SharedPtr m_getIntService;
     rclcpp::Service<blackboard_interfaces::srv::SetStringBlackboard>::SharedPtr m_setStringService;
     rclcpp::Service<blackboard_interfaces::srv::GetStringBlackboard>::SharedPtr m_getStringService;
     std::mutex m_mutexDouble;
+    std::mutex m_mutexInt;
     std::mutex m_mutexString;
     std::map<std::string, std::string> m_stringBlacboard;
     std::map<std::string, double> m_doubleBlacboard;
+    std::map<std::string, int32_t> m_intBlacboard;
 
 };
--- laboratory-tour/src/components/blackboard_component/src/BlackboardComponent.cpp
+++ laboratory-tour/src/components/blackboard_component/src/BlackboardComponent.cpp
@@ -26,6 +26,16 @@ bool BlackboardComponent::start(int argc, char*argv[])
                                                                                 this,
                                                                                 std::placeholders::_1,
                                                                                 std::placeholders::_2));
+    m_setIntService = m_node->create_service<blackboard_interfaces::srv::SetIntBlackboard>("/BlackboardComponent/SetInt",  
+                                                                                std::bind(&BlackboardComponent::SetInt,
+                                                                                this,
+                                                                                std::placeholders::_1,
+                                                                                std::placeholders::_2));
+    m_getIntService = m_node->create_service<blackboard_interfaces::srv::GetIntBlackboard>("/BlackboardComponent/GetInt",  
+                                                                                std::bind(&BlackboardComponent::GetInt,
+                                                                                this,
+                                                                                std::placeholders::_1,
+                                                                                std::placeholders::_2));
     m_setStringService = m_node->create_service<blackboard_interfaces::srv::SetStringBlackboard>("/BlackboardComponent/SetString",  
                                                                                 std::bind(&BlackboardComponent::SetString,
                                                                                 this,
@@ -53,15 +63,15 @@ void BlackboardComponent::spin()
     rclcpp::spin(m_node);  
 }
 
-void BlackboardComponent::GetDouble([[maybe_unused]] const std::shared_ptr<blackboard_interfaces::srv::GetDoubleBlackboard::Request> request,
-            [[maybe_unused]] std::shared_ptr<blackboard_interfaces::srv::GetDoubleBlackboard::Response>      response) 
+void BlackboardComponent::GetDouble( const std::shared_ptr<blackboard_interfaces::srv::GetDoubleBlackboard::Request> request,
+             std::shared_ptr<blackboard_interfaces::srv::GetDoubleBlackboard::Response>      response) 
 {
     std::lock_guard<std::mutex> lock(m_mutexDouble);
     if (request->field_name == "") {
         response->is_ok = false;
         response->error_msg = "missing required field name";
     } else {
-        if (!m_doubleBlacboard.contains(request->field_name)) {
+        if (m_doubleBlacboard.contains(request->field_name)) {
             response->is_ok = false;
             response->error_msg = "field not found";
         } else {
@@ -72,46 +82,8 @@ void BlackboardComponent::GetDouble([[maybe_unused]] const std::shared_ptr<black
 }
 
 
-
-void BlackboardComponent::GetString([[maybe_unused]] const std::shared_ptr<blackboard_interfaces::srv::GetStringBlackboard::Request> request,
-            [[maybe_unused]] std::shared_ptr<blackboard_interfaces::srv::GetStringBlackboard::Response>      response) 
-{
-    std::lock_guard<std::mutex> lock(m_mutexString);
-    if (request->field_name == "") {
-        response->is_ok = false;
-        response->error_msg = "missing required field name";
-    } else if (!m_stringBlacboard.contains(request->field_name)) {
-        response->is_ok = false;
-        response->error_msg = "field not found";
-    } else {
-        response->value = m_stringBlacboard.find(request->field_name)->second;  
-        response->is_ok = true;
-    }
-    
-}
-
-void BlackboardComponent::SetString([[maybe_unused]] const std::shared_ptr<blackboard_interfaces::srv::SetStringBlackboard::Request> request,
-            [[maybe_unused]] std::shared_ptr<blackboard_interfaces::srv::SetStringBlackboard::Response>      response) 
-{
-    std::lock_guard<std::mutex> lock(m_mutexString);
-    if (request->field_name == "") {
-        response->is_ok = false;
-        response->error_msg = "missing required field name";
-    } else if (request->value == "") {
-        response->is_ok = false;
-        response->error_msg = "missing required value";
-    } else {
-        if (!m_stringBlacboard.contains(request->field_name)) {
-            response->error_msg = "field already present, overwriting";
-        } 
-        m_stringBlacboard.insert_or_assign(request->field_name, request->value);  
-        response->is_ok = true;
-    }
-}
-
-
-void BlackboardComponent::SetDouble([[maybe_unused]] const std::shared_ptr<blackboard_interfaces::srv::SetDoubleBlackboard::Request> request,
-            [[maybe_unused]] std::shared_ptr<blackboard_interfaces::srv::SetDoubleBlackboard::Response>      response) 
+void BlackboardComponent::SetDouble( const std::shared_ptr<blackboard_interfaces::srv::SetDoubleBlackboard::Request> request,
+             std::shared_ptr<blackboard_interfaces::srv::SetDoubleBlackboard::Response>      response) 
 {
     std::lock_guard<std::mutex> lock(m_mutexDouble);
     if (request->field_name == "") {
@@ -121,10 +93,86 @@ void BlackboardComponent::SetDouble([[maybe_unused]] const std::shared_ptr<black
         response->is_ok = false;
         response->error_msg = "missing required value";
     } else {
-        if (!m_doubleBlacboard.contains(request->field_name)) {
+        if (m_doubleBlacboard.contains(request->field_name)) {
             response->error_msg = "field already present, overwriting";
         } 
         m_doubleBlacboard.insert_or_assign(request->field_name, request->value);  
+        response->is_ok = true;
+    }
+}
+
+
+void BlackboardComponent::GetString( const std::shared_ptr<blackboard_interfaces::srv::GetStringBlackboard::Request> request,
+             std::shared_ptr<blackboard_interfaces::srv::GetStringBlackboard::Response>      response) 
+{
+    std::lock_guard<std::mutex> lock(m_mutexString);
+    if (request->field_name == "") {
+        response->is_ok = false;
+        response->error_msg = "missing required field name";
+    } else if (m_stringBlacboard.contains(request->field_name)) {
+        response->is_ok = false;
+        response->error_msg = "field not found";
+    } else {
+        response->value = m_stringBlacboard.find(request->field_name)->second;  
+        response->is_ok = true;
+    }
+    
+}
+
+void BlackboardComponent::SetString(const std::shared_ptr<blackboard_interfaces::srv::SetStringBlackboard::Request> request,
+            std::shared_ptr<blackboard_interfaces::srv::SetStringBlackboard::Response>      response) 
+{
+    std::lock_guard<std::mutex> lock(m_mutexString);
+    if (request->field_name == "") {
+        response->is_ok = false;
+        response->error_msg = "missing required field name";
+    } else if (request->value == "") {
+        response->is_ok = false;
+        response->error_msg = "missing required value";
+    } else {
+        if (m_stringBlacboard.contains(request->field_name)) {
+            response->error_msg = "field already present, overwriting";
+        } 
+        m_stringBlacboard.insert_or_assign(request->field_name, request->value);  
+        response->is_ok = true;
+    }
+}
+
+
+void BlackboardComponent::GetInt( const std::shared_ptr<blackboard_interfaces::srv::GetIntBlackboard::Request> request,
+             std::shared_ptr<blackboard_interfaces::srv::GetIntBlackboard::Response>      response) 
+{
+    std::lock_guard<std::mutex> lock(m_mutexInt);
+    if (request->field_name == "") {
+        response->is_ok = false;
+        response->error_msg = "missing required field name";
+    } else {
+        if (m_intBlacboard.contains(request->field_name)) {
+            response->is_ok = false;
+            response->error_msg = "field not found";
+        } else {
+            response->value = m_intBlacboard.find(request->field_name)->second;  
+            response->is_ok = true;
+        }
+    }
+}
+
+
+void BlackboardComponent::SetInt( const std::shared_ptr<blackboard_interfaces::srv::SetIntBlackboard::Request> request,
+             std::shared_ptr<blackboard_interfaces::srv::SetIntBlackboard::Response>      response) 
+{
+    std::lock_guard<std::mutex> lock(m_mutexInt);
+    if (request->field_name == "") {
+        response->is_ok = false;
+        response->error_msg = "missing required field name";
+    } else if (request->value == std::numeric_limits<int32_t>::quiet_NaN()) {
+        response->is_ok = false;
+        response->error_msg = "missing required value";
+    } else {
+        if (m_intBlacboard.contains(request->field_name)) {
+            response->error_msg = "field already present, overwriting";
+        } 
+        m_intBlacboard.insert_or_assign(request->field_name, request->value);  
         response->is_ok = true;
     }
 }
