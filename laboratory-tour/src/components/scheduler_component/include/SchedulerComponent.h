10d1610b131acc69d8c646dd565971d9d29f134e
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-02-14 13:41:40 +0100

    added scheduler interfaces and component
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

---
+++ laboratory-tour/src/components/scheduler_component/include/SchedulerComponent.h
@@ -0,0 +1,40 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2020 Fondazione Istituto Italiano di Tecnologia (IIT)        *
+ * All Rights Reserved.                                                       *
+ *                                                                            *
+ ******************************************************************************/
+
+#include <mutex>
+#include <rclcpp/rclcpp.hpp>
+#include <scheduler_interfaces/srv/update_poi.hpp>
+#include <scheduler_interfaces/srv/reset.hpp>
+#include <scheduler_interfaces/srv/get_current_poi.hpp>
+#include <map>
+#include "TourStorage.h"
+
+class SchedulerComponent
+{
+public:
+    SchedulerComponent() = default;
+
+    bool start(int argc, char*argv[]);
+
+    bool close();
+    void spin();
+    void UpdatePoi([[maybe_unused]] const std::shared_ptr<scheduler_interfaces::srv::UpdatePoi::Request> request,
+                std::shared_ptr<scheduler_interfaces::srv::UpdatePoi::Response>      response);
+    void GetCurrentPoi([[maybe_unused]] const std::shared_ptr<scheduler_interfaces::srv::GetCurrentPoi::Request> request,
+                std::shared_ptr<scheduler_interfaces::srv::GetCurrentPoi::Response>      response);
+    void Reset( [[maybe_unused]] const std::shared_ptr<scheduler_interfaces::srv::Reset::Request> request,
+                std::shared_ptr<scheduler_interfaces::srv::Reset::Response>      response);
+
+private:
+    rclcpp::Node::SharedPtr m_node;
+    rclcpp::Service<scheduler_interfaces::srv::UpdatePoi>::SharedPtr m_updatePoiService;
+    rclcpp::Service<scheduler_interfaces::srv::GetCurrentPoi>::SharedPtr m_getCurrentPoiService;
+    rclcpp::Service<scheduler_interfaces::srv::Reset>::SharedPtr m_resetService;
+    std::mutex m_mutex;
+    int32_t m_currentPoi;
+    std::shared_ptr<TourStorage> m_tourStorage;
+};
