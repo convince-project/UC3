d782d9e5a58014f66ea38d786203e2cebad650cf
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-02-28 15:36:10 +0100

    updated test state machines working

--- test_sm/src/test_sm/src/main.cpp
+++ test_sm/src/test_sm/src/main.cpp
@@ -4,39 +4,55 @@
 #include <string>
 
 #include <iostream>
-#include "StateMachineToTest.h"
+#include "StateMachineToTestSM.h"
 #include "TestStateMachineDataModel.h"
 
 #include <thread>
 #include <chrono>
 
-
-
-
-int main(int argc, char *argv[])
-{
-  StateMachineToTest stateMachineToTest;
+void startApp(StateMachineToTestSM* stateMachineToTest) {
   std::string eventName;
-  TestStateMachineDataModel dataModel;
-    stateMachineToTest.setDataModel(&dataModel);
-      stateMachineToTest.start();
-
-  stateMachineToTest.connectToEvent("SEND_OUTSIDE", [](const QScxmlEvent &){
-    std::cout << "received!";
-
-  });
+  for (const auto& state : stateMachineToTest->activeStateNames()) {
+      std::cout  << "active state: " << state.toStdString() << std::endl;    
+  }  
   do
   {
     std::cin >> eventName;
 
     if (eventName == "CMD_START"){
-      stateMachineToTest.submitEvent("CMD_START");
+      std::cout << "sending event CMD_START" << std::endl;
+      stateMachineToTest->submitEvent("CMD_START");
     } else if (eventName == "CMD_ALARM") {
-      stateMachineToTest.submitEvent("CMD_ALARM");
+      std::cout << "sending event CMD_ALARM" << std::endl;
+      stateMachineToTest->submitEvent("CMD_ALARM");
     } else if (eventName == "CMD_HALT") {
-      stateMachineToTest.submitEvent("CMD_HALT");
+      std::cout << "sending event CMD_HALT" << std::endl;
+      stateMachineToTest->submitEvent("CMD_HALT");
     } 
+    std::this_thread::sleep_for (std::chrono::milliseconds(200));
+
+    for (const auto& state : stateMachineToTest->activeStateNames()) {
+        std::cout  << "active state: " << state.toStdString() << std::endl;    
+    }  
   } while (eventName != "exit");
+}
+
+int main(int argc, char *argv[])
+{
+  QCoreApplication app(argc, argv);
+  StateMachineToTestSM stateMachineToTest;
+  TestStateMachineDataModel dataModel;
+  stateMachineToTest.setDataModel(&dataModel);
+  stateMachineToTest.start();
+
+  stateMachineToTest.connectToEvent("SEND_OUTSIDE", [](const QScxmlEvent &){
+    std::cout << "received!" << std::endl;
+  });
+
+  auto m_threadSpin = std::make_shared<std::thread>(startApp, &stateMachineToTest);
+  app.exec();
+
+
     
   return 0;
   
