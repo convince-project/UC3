3c2780336c9e46abf2ece36fa76d1c98f0b2679c
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-04-03 19:51:37 +0200

    updated launch file with dummy skills
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- laboratory-tour/launch/bt_launch.py
+++ laboratory-tour/launch/bt_launch.py
@@ -7,30 +7,88 @@ def generate_launch_description():
         Node(
             package='alarm_battery_low_skill',
             executable='alarm_battery_low_skill'
-            # output='screen',
-            # arguments=['--ros-args', '--log-level', 'debug']
-
-        ),
-        Node(
-            package='battery_drainer_skill',
-            executable='battery_drainer_skill'
-            # output='screen',
-            # arguments=['--ros-args', '--log-level', 'debug']
         ),
         Node(
             package='battery_level_skill',
             executable='battery_level_skill'
-            # output='screen',
-            # arguments=['--ros-args', '--log-level', 'debug']
         ),
         Node(
-            package='battery_drainer_component',
-            executable='battery_drainer_component'
+            package='alarm_component',
+            executable='alarm_component'
         ),
         Node(
             package='bt_executable',
             executable='bt_executable',
-            arguments=[ './src/behavior_tree/BT/simple_bt.xml']
+            arguments=[ './src/behavior_tree/BT/laboratory_tourv1.xml']
+        ), 
+        Node(
+            package='dummy_condition',
+            executable='dummy_condition', 
+            arguments=['--skill_name', 'BatteryCharging', '--default_status', 'FAILURE']
+        ),         
+        Node(
+            package='dummy_condition',
+            executable='dummy_condition', 
+            arguments=['--skill_name', 'IsAtChargingStation', '--default_status', 'SUCCESS']
+        ),         
+        Node(
+            package='dummy_condition',
+            executable='dummy_condition', 
+            arguments=['--skill_name', 'GoToChargingStation', '--default_status', 'SUCCESS']
+        ),         
+        Node(
+            package='dummy_condition',
+            executable='dummy_condition', 
+            arguments=['--skill_name', 'NetworkDown', '--default_status', 'FAILURE']
+        ),         
+        Node(
+            package='dummy_condition',
+            executable='dummy_condition', 
+            arguments=['--skill_name', 'AtCurrentPOI', '--default_status', 'SUCCESS']
+        ),         
+        Node(
+            package='dummy_condition',
+            executable='dummy_condition', 
+            arguments=['--skill_name', 'VisitorsFollowingRobot', '--default_status', 'FAILURE']
+        ),          
+        Node(
+            package='dummy_condition',
+            executable='dummy_condition', 
+            arguments=['--skill_name', 'IsCheckingForPeople', '--default_status', 'FAILURE']
+        ),
+        Node(
+            package='dummy_action',
+            executable='dummy_action', 
+            arguments=['--skill_name', 'NarratePOI', '--default_status', 'SUCCESS']
+        ), 
+        Node(
+            package='dummy_action',
+            executable='dummy_action', 
+            arguments=['--skill_name', 'UpdatePOI', '--default_status', 'SUCCESS']        ), 
+        Node(
+            package='dummy_action',
+            executable='dummy_action', 
+            arguments=['--skill_name', 'ManageModulesIfNetworkDown', '--default_status', 'SUCCESS']
+        ),
+        Node(
+            package='dummy_action',
+            executable='dummy_action', 
+            arguments=['--skill_name', 'SayWhileNavigating', '--default_status', 'SUCCESS']
+        ), 
+        Node(
+            package='dummy_action',
+            executable='dummy_action', 
+            arguments=['--skill_name', 'GoToPOI', '--default_status', 'SUCCESS']
+        ), 
+        Node(
+            package='dummy_action',
+            executable='dummy_action', 
+            arguments=['--skill_name', 'SayFollowMe', '--default_status', 'SUCCESS']
+        ), 
+        Node(
+            package='dummy_action',
+            executable='dummy_action', 
+            arguments=['--skill_name', 'StopAndTurnBack', '--default_status', 'SUCCESS']
         )
     ])
 
