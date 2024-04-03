23e8023e0eec35030cc50d88d06c08d1e9e76b52
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-04-03 19:23:40 +0200

    updated dummy condition to take also default status
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- laboratory-tour/src/skills/dummy_condition/src/main.cpp
+++ laboratory-tour/src/skills/dummy_condition/src/main.cpp
@@ -18,8 +18,14 @@ int main(int argc, char *argv[])
         RCLCPP_ERROR(rclcpp::get_logger("DummyConditionLogger"), "Missing skill_name parameter");
         return 1;
     }
+    if(!rf.check("default_status"))
+    {
+        std::cout << "ERROR: Missing default_status parameter\n";
+        RCLCPP_ERROR(rclcpp::get_logger("DummyConditionLogger"), "Missing default_status parameter");
+        return 1;
+    }
     rclcpp::init(/*argc*/ argc, /*argv*/ argv);
-    std::shared_ptr<DummyCondition> dummyCondition = std::make_shared<DummyCondition>(rf.find("skill_name").asString());
+    std::shared_ptr<DummyCondition> dummyCondition = std::make_shared<DummyCondition>(rf.find("skill_name").asString(), rf.find("default_status").asString());
     rclcpp::spin(dummyCondition);
     rclcpp::shutdown();
 
