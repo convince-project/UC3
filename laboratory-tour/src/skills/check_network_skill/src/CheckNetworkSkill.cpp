c89505c97e5bc3499eb978bea6134197d136a369
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-04-05 10:36:39 +0200

    refactored check network skill
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- laboratory-tour/src/skills/check_network_skill/src/CheckNetworkSkill.cpp
+++ laboratory-tour/src/skills/check_network_skill/src/CheckNetworkSkill.cpp
@@ -16,7 +16,6 @@
 CheckNetworkSkill::CheckNetworkSkill(std::string name ) :
         m_name(std::move(name))
 {
-    m_dataModel.set_name(name+"dataModel");
     m_stateMachine.setDataModel(&m_dataModel);
 }
 
@@ -36,10 +35,10 @@ bool CheckNetworkSkill::start(int argc, char*argv[])
         rclcpp::init(/*argc*/ argc, /*argv*/ argv);
     }
 
-    m_node = rclcpp::Node::make_shared(m_name);
+    m_node = rclcpp::Node::make_shared(m_name + "Skill");
 
     
-    RCLCPP_DEBUG(m_node->get_logger(), "CheckNetworkSkill::start");
+    RCLCPP_DEBUG_STREAM(m_node->get_logger(), "CheckNetworkSkill::start");
     std::cout << "CheckNetworkSkill::start";
     m_tickService = m_node->create_service<bt_interfaces::srv::TickCondition>(m_name + "Skill/tick",  
                                                                                 std::bind(&CheckNetworkSkill::tick,
@@ -47,36 +46,61 @@ bool CheckNetworkSkill::start(int argc, char*argv[])
                                                                                 std::placeholders::_1,
                                                                                 std::placeholders::_2));
 
-    m_stateMachine.start();
     m_threadSpin = std::make_shared<std::thread>(spin, m_node);
+
+
+
+
+    m_stateMachine.connectToEvent("tickReturn", [this]([[maybe_unused]]const QScxmlEvent & event){
+                qInfo() <<  "CheckNetworkSkill::tick " << __LINE__ ;
+
+        std::string result = event.data().toMap()["result"].toString().toStdString();
+        RCLCPP_INFO(m_node->get_logger(), "CheckNetworkSkill::tickresponse----------------%s--------------------------------", result.c_str());
+
+        if (result == "SUCCESS" )
+        { 
+            m_tickResult.store(Status::success);
+        } else if (result == "FAILURE" )
+        { 
+            m_tickResult.store(Status::failure);
+        }
+    });
+    m_stateMachine.start();
+
     return true;
 }
-
 
 void CheckNetworkSkill::tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces::srv::TickCondition::Request> request,
                                        std::shared_ptr<bt_interfaces::srv::TickCondition::Response>      response)
 {
-    RCLCPP_INFO_STREAM(m_node->get_logger(), m_name << "::tick");    
-    std::cout <<  m_name << "::tick";  
-    bool done = false; 
-    m_stateMachine.submitEvent("CMD_START");
-    std::this_thread::sleep_for (std::chrono::milliseconds(200));
-
+    qInfo() <<  "CheckNetworkSkill::tick " << __LINE__ ;
+    std::lock_guard<std::mutex> lock(m_requestMutex);
+    RCLCPP_INFO(m_node->get_logger(), "CheckNetworkSkill::tick");
     auto message = bt_interfaces::msg::ConditionResponse();
-    do {
-        for (const auto& state : m_stateMachine.activeStateNames()) {
-            RCLCPP_INFO_STREAM(m_node->get_logger(), m_name << "::tick, state " << state.toStdString() );    
-            if (state == "success") {
-                m_stateMachine.submitEvent("CMD_OK");
-                response->status.status = message.SKILL_SUCCESS;
-                response->is_ok = true;
-                done = true;
-            }else if (state == "failure") {
-                m_stateMachine.submitEvent("CMD_OK");
-                response->status.status = message.SKILL_FAILURE;
-                response->is_ok = true;
-                done = true;
-            }
-        }
-    } while (!done);
+    m_tickResult.store(Status::undefined); //here we can put a struct
+
+
+    m_stateMachine.submitEvent("tickCall");
+
+    while(m_tickResult.load()== Status::undefined) 
+    {
+        // qInfo() <<  "active names" << m_stateMachine.activeStateNames();
+        std::this_thread::sleep_for (std::chrono::milliseconds(100));
+    }
+            qInfo() <<  "CheckNetworkSkill::tick " << __LINE__ ;
+
+    switch(m_tickResult.load()) 
+    {
+        case Status::failure:
+            response->status.status = message.SKILL_FAILURE;
+            break;
+        case Status::success:
+            response->status.status = message.SKILL_SUCCESS;
+            break;            
+    }
+    RCLCPP_INFO(m_node->get_logger(), "CheckNetworkSkill::tickDone");
+
+    response->is_ok = true;
 }
+
+
--- laboratory-tour/src/skills/check_network_skill/src/main.cpp
+++ laboratory-tour/src/skills/check_network_skill/src/main.cpp
@@ -2,21 +2,24 @@
 #include <QScxmlStateMachine>
 #include <QDebug>
 
+
 #include <iostream>
 #include "CheckNetworkSkill.h"
 
 #include <thread>
 #include <chrono>
 
+
+
 int main(int argc, char *argv[])
 {
   QCoreApplication app(argc, argv);
-  CheckNetworkSkill stateMachine("CheckNetwork");
+  CheckNetworkSkill stateMachine("BatteryLevel");
   stateMachine.start(argc, argv);
 
   int ret=app.exec();
-
+  
   return ret;
-
+  
 }
 
