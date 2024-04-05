74e1b8b524084feb6f5075477f0dd21e8205b1de
SofiaFaraci <sofia.faraci@iit.it>
2024-04-05 12:24:27 +0200

    add working battery_charging_skill
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- laboratory-tour/src/skills/battery_charging_skill/include/BatteryChargingSkill.h
+++ laboratory-tour/src/skills/battery_charging_skill/include/BatteryChargingSkill.h
@@ -7,30 +7,38 @@
 
 # pragma once
 
+#include <mutex>
 #include <thread>
 #include <rclcpp/rclcpp.hpp>
 #include "BatteryChargingSkillSM.h"
-#include "BatteryChargingDataModel.h"
 #include <bt_interfaces/msg/condition_response.hpp>
 #include <bt_interfaces/srv/tick_condition.hpp>
-#include <mutex>
+#include "BatteryChargingSkillDataModel.h"
+
+enum class Status{
+	undefined,
+	success,
+	failure
+};
 
 class BatteryChargingSkill
 {
 public:
-    BatteryChargingSkill(std::string name );
-
-    bool start(int argc, char * argv[]);
-    static void spin(std::shared_ptr<rclcpp::Node> node);
-    void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces::srv::TickCondition::Request> request,
-          std::shared_ptr<bt_interfaces::srv::TickCondition::Response>      response);
+	BatteryChargingSkill(std::string name );
+	bool start(int argc, char * argv[]);
+	static void spin(std::shared_ptr<rclcpp::Node> node);
+	void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces::srv::TickCondition::Request> request,
+			   std::shared_ptr<bt_interfaces::srv::TickCondition::Response>      response);
 
 private:
-    std::shared_ptr<std::thread> m_threadSpin;
-    std::shared_ptr<rclcpp::Node> m_node;
-    rclcpp::Service<bt_interfaces::srv::TickCondition>::SharedPtr m_tickService;
-    std::string m_name;
-    BatteryChargingDataModel m_dataModel;
-    BatteryChargingSkillSM m_stateMachine;
-    std::mutex m_requestMutex;
+	std::shared_ptr<std::thread> m_threadSpin;
+	std::shared_ptr<rclcpp::Node> m_node;
+	std::mutex m_requestMutex;
+	std::string m_name;
+	BatteryChargingSkillCondition m_stateMachine;
+	BatteryChargingSkillDataModel m_dataModel;
+	std::atomic<Status> m_tickResult{Status::undefined};
+	rclcpp::Service<bt_interfaces::srv::TickCondition>::SharedPtr m_tickService;
+	
 };
+
