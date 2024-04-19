213918e1415eba42074d4747c6cdd408beb54579
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-04-19 11:22:37 +0200

    added narrate component

--- laboratory-tour/src/components/narrate_component/NarrateComponent.py
+++ laboratory-tour/src/components/narrate_component/NarrateComponent.py
@@ -2,12 +2,15 @@ import rclpy
 from rclpy.node import Node
 from std_srvs.srv import Trigger
 from scheduler_interfaces.srv import GetCurrentAction, UpdateAction
-from text_to_speech_interfaces.srv import Speak
+from text_to_speech_interfaces.srv import Speak, IsDone
+import threading
 
-class MyService(Node):
+global done
 
+class NodeExecutor(Node):
     def __init__(self):
-        super().__init__('NarrateComponentNode')
+        super().__init__(node_name = 'NarrateComponentThreadNode')
+
         self.client = self.create_client(GetCurrentAction, '/SchedulerComponent/GetCurrentAction')
         while not self.client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Service not available, waiting...')
@@ -20,9 +23,11 @@ class MyService(Node):
         while not self.client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Service not available, waiting...')
 
-        self.service = self.create_service(Trigger, '/NarrateComponent/Narrate', self.callback)
+        self.client = self.create_client(IsDone, '/TextToSpeechComponent/IsDone')
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Service not available, waiting...')
 
-    def callback(self, request, response):
+    def run():
         done_with_poi = False
         while not done_with_poi:
             # call the GetCurrentAction service
@@ -30,31 +35,63 @@ class MyService(Node):
             future_getCurrentAction = self.client.call_async(request_getCurrentAction)
             rclpy.spin_until_future_complete(self, future_getCurrentAction)
             if future_getCurrentAction.result() is not None:
-                response.text = future_getCurrentAction.result().action
-                self.get_logger().info('Response: {0}'.format(response.text))
+                text_to_send = future_getCurrentAction.result().action
+                self.get_logger().info('Response: {0}'.format(text_to_send))
 
                 # call the text-to-speech service
                 request_speak = Speak.Request()
-                request_speak.text = response.text
+                request_speak.text = text_to_send
                 future_speak = self.client.call_async(request_speak)
                 rclpy.spin_until_future_complete(self, future_speak)
                 if future_speak.result() is not None:
-                    response.text = future_speak.result().text
-                    self.get_logger().info('Response: {0}'.format(response.text))
+                    if future_speak.result().is_ok:
+                        is_done = False
+                        while not is_done:
+                            # call the IsDone service
+                            request_isDone = IsDone.Request()
+                            future_isDone = self.client.call_async(request_isDone)
+                            rclpy.spin_until_future_complete(self, future_isDone)
+                            if future_isDone.result() is not None:
+                                is_done = future_isDone.result().is_done
+                            #sleep for 150 ms
+                            self.sleep(0.15)
 
             #call the UpdateAction service
             request_updateAction = UpdateAction.Request()
             future_updateAction = self.client.call_async(request_updateAction)
             rclpy.spin_until_future_complete(self, future_updateAction)
-            if future_updateAction.result() is not None:
-                response.text = future_updateAction.result().action
-                self.get_logger().info('Response: {0}'.format(response.text))         
+            if future_updateAction.result() is not None:       
                 done_with_poi = future_updateAction.result().done_with_poi
+        done = True
+
+
+
+
+class MyService(Node):
+
+    def __init__(self):
+        super().__init__('NarrateComponentNode')
+        self.nodeExecutor = NodeExecutor()
+        self.service = self.create_service(Trigger, '/NarrateComponent/Narrate', self.callback)
+        self.service = self.create_service(Trigger, '/NarrateComponent/IsDone', self.callbackIsDone)
+
+    def callback(self, request, response):
+        # launch the thread
+        done = False
+        # launch NodeExecutor class with lambda
+        nodeExecutorInstance = threading.Thread(target=lambda: self.nodeExecutor.run())
         response.success = True
         return response
     
+    def callbackIsDone(self, request, response):
+        response.success = done
+        return response
 
-    
+
+# create a thread class that executes the main function
+
+
+
 def main(args=None):
     rclpy.init(args=args)
     node = MyService()
