6277eb35757532cc07d9bd39836d77627f09b710
SofiaFaraci <sofia.faraci@iit.it>
2024-04-19 13:22:49 +0200

    updated narrate_skill

---
+++ laboratory-tour/src/skills/narrate_skill/include/NarrateSkill.h
@@ -0,0 +1,51 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2020 Fondazione Istituto Italiano di Tecnologia (IIT)        *
+ * All Rights Reserved.                                                       *
+ *                                                                            *
+ ******************************************************************************/
+
+# pragma once
+
+#include <mutex>
+#include <thread>
+#include <rclcpp/rclcpp.hpp>
+#include "NarrateSkillSM.h"
+#include <bt_interfaces/msg/action_response.hpp>
+#include <narrate_interfaces/srv/narrate.hpp>
+#include <narrate_interfaces/srv/narrate.hpp>
+#include <bt_interfaces/srv/tick_action.hpp>
+#include <bt_interfaces/srv/halt_action.hpp>
+
+enum class Status{
+	undefined,
+	running,
+	success,
+	failure
+};
+
+class NarrateSkill
+{
+public:
+	NarrateSkill(std::string name );
+	bool start(int argc, char * argv[]);
+	static void spin(std::shared_ptr<rclcpp::Node> node);
+	void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces::srv::TickAction::Request> request,
+			   std::shared_ptr<bt_interfaces::srv::TickAction::Response>      response);
+	void halt( [[maybe_unused]] const std::shared_ptr<bt_interfaces::srv::HaltAction::Request> request,
+			   [[maybe_unused]] std::shared_ptr<bt_interfaces::srv::HaltAction::Response> response);
+
+private:
+	std::shared_ptr<std::thread> m_threadSpin;
+	std::shared_ptr<rclcpp::Node> m_node;
+	std::mutex m_requestMutex;
+	std::string m_name;
+	NarrateSkillAction m_stateMachine;
+	
+	std::atomic<Status> m_tickResult{Status::undefined};
+	rclcpp::Service<bt_interfaces::srv::TickAction>::SharedPtr m_tickService;
+	std::atomic<bool> m_haltResult{false};
+	rclcpp::Service<bt_interfaces::srv::HaltAction>::SharedPtr m_haltService;
+	
+};
+
