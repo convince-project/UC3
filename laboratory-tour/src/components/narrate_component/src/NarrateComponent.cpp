3ffc570ef5381e7111819bd33866dc5bc846786b
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-04-29 17:31:34 +0200

    updated narrate component, to be tested
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- laboratory-tour/src/components/narrate_component/src/NarrateComponent.cpp
+++ laboratory-tour/src/components/narrate_component/src/NarrateComponent.cpp
@@ -30,13 +30,6 @@ bool NarrateComponent::start(int argc, char*argv[])
                                                                                 std::placeholders::_1,
                                                                                 std::placeholders::_2));
 
-    //setup the subscription to the DoneSpeaking message
-    m_doneSpeakingSub = m_node->create_subscription<text_to_speech_interfaces::msg::DoneSpeaking>("/TextToSpeech/DoneSpeaking", 
-                                                                                                    10, 
-                                                                                                    [this](const text_to_speech_interfaces::msg::DoneSpeaking::SharedPtr msg) {
-                                                                                                        m_isDoneSpeaking = msg->is_done;
-                                                                                                    });
-
     RCLCPP_DEBUG(m_node->get_logger(), "NarrateComponent::start");
     std::cout << "NarrateComponent::start";        
     return true;
@@ -60,7 +53,7 @@ void NarrateComponent::spin()
 void NarrateComponent::IsDone([[maybe_unused]] const std::shared_ptr<narrate_interfaces::srv::IsDone::Request> request,
              std::shared_ptr<narrate_interfaces::srv::IsDone::Response>      response) 
 {
-    response->is_done = m_doneWithPoi;
+    response->is_done = !m_doneWithPoi;
     response->is_ok = true;
 }
 
@@ -89,35 +82,45 @@ void NarrateComponent::Narrate(const std::shared_ptr<narrate_interfaces::srv::Na
             if (currentAction->type == "0") // 0 is speak
     
             {
-                // // calls the Speak service
-                // auto speakRequest = std::make_shared<text_to_speech_interfaces::srv::Speak::Request>();
-                // speakRequest->text = currentAction->param;
-                // auto speakResult = m_speakClient->async_send_request(speakRequest);
-                // while (rclcpp::ok() && rclcpp::spin_until_future_complete(m_node, speakResult) != rclcpp::FutureReturnCode::SUCCESS)
-                // {
-                //     if (!rclcpp::ok())
-                //     {
-                //         RCLCPP_ERROR(m_node->get_logger(), "Interrupted while waiting for the result.");
-                //         return;
-                //     } else {
-                //         RCLCPP_INFO(m_node->get_logger(), "Waiting for the result.");
-                //     }
-                //     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                // }
-                // prints the text to the console
-                std::cout << "NarrateComponent::Narratetype: " << currentAction->type << std::endl;
-                std::cout << "NarrateComponent::Narrate: " << currentAction->param << std::endl;
-                RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "NarrateComponent::Narrate: " << currentAction->param );
-                std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+                auto speakClientNode = rclcpp::Node::make_shared("NarrateComponentSpeakNode");
+                std::shared_ptr<rclcpp::Client<text_to_speech_interfaces::srv::Speak>> speakClient = 
+                speakClientNode->create_client<text_to_speech_interfaces::srv::Speak>("/TextToSpeechComponent/Speak");
+                auto speakRequest = std::make_shared<text_to_speech_interfaces::srv::Speak::Request>();
+                speakRequest->text = currentAction->param;
+
+                while (!speakClient->wait_for_service(std::chrono::seconds(1))) {
+                    if (!rclcpp::ok()) {
+                        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service 'speakClient'. Exiting.");
+                    }
+                }
+                auto speakResult = speakClient->async_send_request(speakRequest);
+                auto futureSpeakResult = rclcpp::spin_until_future_complete(speakClientNode, speakResult);
+                auto speakFutureResult = speakResult.get();
+                if (speakFutureResult->is_ok == true) { // 0 is speak
+                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                }
             }
 
-            // //waits for the DoneSpeaking message
-            // bool m_isDoneSpeaking = false;
-            // while (!m_isDoneSpeaking)
-            // {
-            //     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            // }
-
+            // waits until the robot has finished speaking
+            bool isSpeaking = false;
+            do {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                // calls the isSpeaking service
+                auto isSpeakingClientNode = rclcpp::Node::make_shared("NarrateComponentIsSpeakingNode");
+                std::shared_ptr<rclcpp::Client<text_to_speech_interfaces::srv::IsSpeaking>> isSpeakingClient =
+                isSpeakingClientNode->create_client<text_to_speech_interfaces::srv::IsSpeaking>("/TextToSpeechComponent/IsSpeaking");
+                auto isSpeakingRequest = std::make_shared<text_to_speech_interfaces::srv::IsSpeaking::Request>();
+                while (!isSpeakingClient->wait_for_service(std::chrono::seconds(1))) {
+                    if (!rclcpp::ok()) {
+                        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service 'isSpeakingClient'. Exiting.");
+                    }
+                }
+                auto isSpeakingResult = isSpeakingClient->async_send_request(isSpeakingRequest);
+                auto futureIsSpeakingResult = rclcpp::spin_until_future_complete(isSpeakingClientNode, isSpeakingResult);
+                auto isSpeakingResponse = isSpeakingResult.get();
+                isSpeaking = !isSpeakingResponse->is_speaking;
+                
+            } while (isSpeaking);
             
             // calls the UpdateAction service
             auto updateActionClientNode = rclcpp::Node::make_shared("NarrateComponentUpdateActionNode");
