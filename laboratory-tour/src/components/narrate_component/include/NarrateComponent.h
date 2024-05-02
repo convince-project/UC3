22404fd2a564178521f6afdac1a09f1bc84827b0
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-05-02 09:42:22 +0200

    updated narrate component to accept stop, to be tested
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- laboratory-tour/src/components/narrate_component/include/NarrateComponent.h
+++ laboratory-tour/src/components/narrate_component/include/NarrateComponent.h
@@ -12,6 +12,7 @@
 
 #include <narrate_interfaces/srv/narrate.hpp>
 #include <narrate_interfaces/srv/is_done.hpp>
+#include <narrate_interfaces/srv/stop.hpp>
 #include <scheduler_interfaces/srv/update_action.hpp>
 #include <scheduler_interfaces/srv/get_current_action.hpp>
 #include <scheduler_interfaces/srv/set_command.hpp>
@@ -33,16 +34,20 @@ public:
                 std::shared_ptr<narrate_interfaces::srv::Narrate::Response>      response);
     void IsDone([[maybe_unused]] const std::shared_ptr<narrate_interfaces::srv::IsDone::Request> request,
                 std::shared_ptr<narrate_interfaces::srv::IsDone::Response>      response);
+    void Stop([[maybe_unused]] const std::shared_ptr<narrate_interfaces::srv::Stop::Request> request,
+                std::shared_ptr<narrate_interfaces::srv::Stop::Response>      response);
 
 private:
     rclcpp::Node::SharedPtr m_node;
     rclcpp::Service<narrate_interfaces::srv::Narrate>::SharedPtr m_narrateService;
     rclcpp::Service<narrate_interfaces::srv::IsDone>::SharedPtr m_isDoneService;
+    rclcpp::Service<narrate_interfaces::srv::Stop>::SharedPtr m_stopService;
 
     // rclcpp::Client<text_to_speech_interfaces::srv::Speak>::SharedPtr m_speakClient;
     std::mutex m_mutex;
     int32_t m_currentPoi;
     bool m_doneWithPoi{false};
     bool m_isDoneSpeaking{false};
+    bool m_stopped = false;
     std::thread m_threadNarration;
 };
