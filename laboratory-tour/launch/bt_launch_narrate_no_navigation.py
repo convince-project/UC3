8aa7eed9bb77a2e2d20e10278542d67709b34b3b
stefano bernagozzi <stefano.bernagozzi@iit.it>
2024-05-08 13:11:47 +0000

    update in BTs
    Signed-off-by: stefano bernagozzi <stefano.bernagozzi@iit.it>
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

---
+++ laboratory-tour/launch/bt_launch_narrate_no_navigation.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='bt_executable',
+            executable='bt_executable',
+            arguments=[ './src/behavior_tree/BT/bt_narrate_no_navigation.xml']
+        ), 
+    ])
+
