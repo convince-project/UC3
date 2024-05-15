f6b8e89591e9ad69b5e4b69e3242098e83347849
SofiaFaraci <sofia.faraci@iit.it>
2024-05-15 15:58:22 +0200

    add hysteresis like thresholds
    Signed-off-by: SofiaFaraci <sofia.faraci@iit.it>
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- laboratory-tour/src/skills/battery_level_skill/include/BatteryLevelSkillDataModel.h
+++ laboratory-tour/src/skills/battery_level_skill/include/BatteryLevelSkillDataModel.h
@@ -5,34 +5,38 @@
  *                                                                            *
  ******************************************************************************/
 
-#pragma once
+# pragma once
 
 #include <QScxmlCppDataModel>
 #include <QVariant>
-#include <sensor_msgs/msg/battery_state.hpp>
 #include <string>
-#include <sensor_msgs/msg/battery_state.hpp>
 #include <thread>
+#include <vector>
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/battery_state.hpp>
 
+#define LOW_THRESHOLD   30
+#define HIGH_THRESHOLD  80
+#define VECTOR_SIZE     10
 
 class BatteryLevelSkillDataModel: public QScxmlCppDataModel
 {
     Q_SCXML_DATAMODEL
 
 public:
-    BatteryLevelSkillDataModel() = default;
-    bool setup(const QVariantMap& initialDataValues) override;
-    void log(std::string to_log);
-    void topic_battery_callback(const sensor_msgs::msg::BatteryState::SharedPtr msg);
-    static void spin(std::shared_ptr<rclcpp::Node> node);
-private: 
-    // sensor_msgs::msg::BatteryState m_batteryState;
-    double percentage;
-    rclcpp::Subscription<sensor_msgs::msg::BatteryState>::SharedPtr m_subscription;
-    std::shared_ptr<std::thread> m_threadSpin;
-    std::shared_ptr<rclcpp::Node> m_node;
+   BatteryLevelSkillDataModel() = default;
+   bool setup(const QVariantMap& initialDataValues) override;
+   void log(std::string to_log);
+   void topic_battery_callback(const sensor_msgs::msg::BatteryState::SharedPtr msg);
+   static void spin(std::shared_ptr<rclcpp::Node> node);
 
+private:
+   std::vector<float> m_batteryLevelVector;
+   bool m_returnValue = false;
+   rclcpp::Subscription<sensor_msgs::msg::BatteryState>::SharedPtr m_subscription;
+   std::shared_ptr<std::thread> m_threadSpin;
+   std::shared_ptr<rclcpp::Node> m_node;
+	
 };
 
 Q_DECLARE_METATYPE(::BatteryLevelSkillDataModel*)
