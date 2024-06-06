b802e88840f9d2dc002e17eaf43b2cf4724b2b3a
SofiaFaraci <sofia.faraci@iit.it>
2024-06-06 17:13:01 +0200

    add run_timer_skill
    Signed-off-by: SofiaFaraci <sofia.faraci@iit.it>

---
+++ laboratory-tour/src/skills/run_timer_skill/include/RunTimerSkill.h
@@ -0,0 +1,50 @@
+# pragma once
+
+#include <mutex>
+#include <thread>
+#include <rclcpp/rclcpp.hpp>
+#include "RunTimerSkillSM.h"
+#include <bt_interfaces/msg/action_response.hpp>
+#include <timer_check_for_people_interfaces/srv/start_timer.hpp> 
+#include <timer_check_for_people_interfaces/srv/is_timer_active.hpp> 
+
+
+#include <bt_interfaces/srv/tick_action.hpp>
+#include <bt_interfaces/srv/halt_action.hpp>
+
+
+#define SERVICE_TIMEOUT 8
+
+enum class Status{
+	undefined,
+	running, 
+	success,
+	failure
+};
+
+class RunTimerSkill
+{
+public:
+	RunTimerSkill(std::string name );
+	bool start(int argc, char * argv[]);
+	static void spin(std::shared_ptr<rclcpp::Node> node);
+	
+	void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces::srv::TickAction::Request> request,
+			   std::shared_ptr<bt_interfaces::srv::TickAction::Response>      response);
+	
+	void halt( [[maybe_unused]] const std::shared_ptr<bt_interfaces::srv::HaltAction::Request> request,
+			   [[maybe_unused]] std::shared_ptr<bt_interfaces::srv::HaltAction::Response> response);
+
+private:
+	std::shared_ptr<std::thread> m_threadSpin;
+	std::shared_ptr<rclcpp::Node> m_node;
+	std::mutex m_requestMutex;
+	std::string m_name;
+	RunTimerSkillAction m_stateMachine;
+	std::atomic<Status> m_tickResult{Status::undefined};
+	rclcpp::Service<bt_interfaces::srv::TickAction>::SharedPtr m_tickService;
+	std::atomic<bool> m_haltResult{false};
+	rclcpp::Service<bt_interfaces::srv::HaltAction>::SharedPtr m_haltService;
+	
+};
+
