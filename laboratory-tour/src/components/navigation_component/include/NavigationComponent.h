6c57dc18ea17bd457ec1448b0c339737be722981
SofiaFaraci <sofia.faraci@iit.it>
2024-08-27 15:31:34 +0200

    add go to poi navigation as ros2 action
    Signed-off-by: SofiaFaraci <sofia.faraci@iit.it>

--- laboratory-tour/src/components/navigation_component/include/NavigationComponent.h
+++ laboratory-tour/src/components/navigation_component/include/NavigationComponent.h
@@ -14,11 +14,14 @@
 #include <yarp/os/RFModule.h>
 #include <yarp/dev/PolyDriver.h>
 #include <yarp/dev/INavigation2D.h>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <navigation_interfaces/msg/navigation_status.hpp>
 #include <navigation_interfaces/srv/go_to_poi_by_name.hpp>
 #include <navigation_interfaces/srv/get_navigation_status.hpp>
 #include <navigation_interfaces/srv/stop_navigation.hpp>
 #include <navigation_interfaces/srv/check_near_to_poi.hpp>
 #include <navigation_interfaces/srv/turn_back.hpp>
+#include <navigation_interfaces/action/go_to_poi.hpp>
 
 
 class NavigationComponent 
@@ -42,6 +45,9 @@ public:
     void TurnBack( [[maybe_unused]] const std::shared_ptr<navigation_interfaces::srv::TurnBack::Request> request,
             std::shared_ptr<navigation_interfaces::srv::TurnBack::Response>      response);
 
+    void Execute(const std::shared_ptr<navigation_interfaces::action::GoToPoi::Goal> goal);
+    void ExecuteCancel(const std::shared_ptr<rclcpp_action::ServerGoalHandle<navigation_interfaces::action::GoToPoi>> goalHandle);
+    
 private:
     yarp::dev::PolyDriver m_nav2DPoly;
     yarp::dev::Nav2D::INavigation2D *m_iNav2D{nullptr};
@@ -51,6 +57,16 @@ private:
     rclcpp::Service<navigation_interfaces::srv::StopNavigation>::SharedPtr m_stopNavigationService;
     rclcpp::Service<navigation_interfaces::srv::CheckNearToPoi>::SharedPtr m_checkNearToPoiService;
     rclcpp::Service<navigation_interfaces::srv::TurnBack>::SharedPtr m_turnBackService;
+
+    std::shared_ptr<rclcpp_action::ServerGoalHandle<navigation_interfaces::action::GoToPoi>> m_activeGoal;
+    std::mutex m_goalMutex;
+
+	rclcpp_action::Server<navigation_interfaces::action::GoToPoi>::SharedPtr m_actionServer;
+    rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID & uuid, std::shared_ptr<const navigation_interfaces::action::GoToPoi::Goal> goal);
+    rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<rclcpp_action::ServerGoalHandle<navigation_interfaces::action::GoToPoi>> goal_handle);
+    void handle_accepted(const std::shared_ptr<rclcpp_action::ServerGoalHandle<navigation_interfaces::action::GoToPoi>> goal_handle);
+    void execute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<navigation_interfaces::action::GoToPoi>> goal_handle);
+    navigation_interfaces::msg::NavigationStatus convertStatus(yarp::dev::Nav2D::NavigationStatusEnum status);
     std::mutex m_mutex;
     int32_t m_currentPoi;
 };
--- laboratory-tour/src/components/navigation_component/src/NavigationComponent.cpp
+++ laboratory-tour/src/components/navigation_component/src/NavigationComponent.cpp
@@ -45,6 +45,12 @@ bool NavigationComponent::start(int argc, char*argv[])
                                                                                 this,
                                                                                 std::placeholders::_1,
                                                                                 std::placeholders::_2));
+    m_actionServer = rclcpp_action::create_server<navigation_interfaces::action::GoToPoi>(
+        m_node,
+        "/NavigationComponent/GoToPoi",
+        std::bind(&NavigationComponent::handle_goal, this, std::placeholders::_1, std::placeholders::_2),
+        std::bind(&NavigationComponent::handle_cancel, this, std::placeholders::_1),
+        std::bind(&NavigationComponent::handle_accepted, this, std::placeholders::_1));
 
     RCLCPP_DEBUG(m_node->get_logger(), "NavigationComponent::start");
     std::cout << "NavigationComponent::start";        
@@ -255,4 +261,174 @@ void NavigationComponent::TurnBack([[maybe_unused]] const std::shared_ptr<naviga
     {
         response->is_ok = true;
     }
+}
+
+
+navigation_interfaces::msg::NavigationStatus NavigationComponent::convertStatus(yarp::dev::Nav2D::NavigationStatusEnum status){
+    auto msg = navigation_interfaces::msg::NavigationStatus();
+    navigation_interfaces::msg::NavigationStatus output;
+    switch (status){
+    case yarp::dev::Nav2D::navigation_status_idle:
+        output.status = msg.NAVIGATION_STATUS_IDLE;
+        break;
+    case yarp::dev::Nav2D::navigation_status_preparing_before_move:
+        output.status = msg.NAVIGATION_STATUS_PREPARING_BEFORE_MOVE;
+        break;
+    case yarp::dev::Nav2D::navigation_status_moving:
+        output.status = msg.NAVIGATION_STATUS_MOVING;
+        break;
+    case yarp::dev::Nav2D::navigation_status_waiting_obstacle:
+        output.status = msg.NAVIGATION_STATUS_WAITING_OBSTACLE;
+        break;
+    case yarp::dev::Nav2D::navigation_status_goal_reached:
+        output.status = msg.NAVIGATION_STATUS_GOAL_REACHED;
+        break;
+    case yarp::dev::Nav2D::navigation_status_aborted:
+        output.status = msg.NAVIGATION_STATUS_ABORTED;
+        break;
+    case yarp::dev::Nav2D::navigation_status_failing:
+        output.status = msg.NAVIGATION_STATUS_FAILING;
+        break;
+    case yarp::dev::Nav2D::navigation_status_paused:
+        output.status = msg.NAVIGATION_STATUS_PAUSED;
+        break;
+    case yarp::dev::Nav2D::navigation_status_thinking:
+        output.status = msg.NAVIGATION_STATUS_THINKING;
+        break;
+    case yarp::dev::Nav2D::navigation_status_error:
+        output.status = msg.NAVIGATION_STATUS_ERROR;
+        break;
+    default:
+        output.status = msg.NAVIGATION_STATUS_ERROR;
+        break;
+    }
+    return output;
+}
+
+rclcpp_action::GoalResponse NavigationComponent::handle_goal(
+        const rclcpp_action::GoalUUID & uuid,
+        std::shared_ptr<const navigation_interfaces::action::GoToPoi::Goal> goal)
+{
+    RCLCPP_INFO(m_node->get_logger(), "GoToPoi Action - Received goal request, poi_name: %s", goal->poi_name.c_str());
+    (void)uuid;
+    if(goal->poi_name != "")
+    {
+        return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+    }
+    else
+    {
+        return rclcpp_action::GoalResponse::REJECT;
+    }
+}
+
+rclcpp_action::CancelResponse NavigationComponent::handle_cancel(const std::shared_ptr<rclcpp_action::ServerGoalHandle<navigation_interfaces::action::GoToPoi>> goal_handle)
+{
+    RCLCPP_INFO(m_node->get_logger(), "GoToPoi Action - Received request to cancel goal");
+    (void)goal_handle;
+    return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+void NavigationComponent::handle_accepted(const std::shared_ptr<rclcpp_action::ServerGoalHandle<navigation_interfaces::action::GoToPoi>> goal_handle)
+{
+    std::lock_guard<std::mutex> lock(m_goalMutex);
+
+    if (m_activeGoal && !m_activeGoal->is_canceling()) {
+        RCLCPP_INFO(m_node->get_logger(), "Preempting the current goal");
+        auto result = std::make_shared<navigation_interfaces::action::GoToPoi::Result>();
+        result->is_ok = false;
+        result->error_msg = "Preempted by a new goal";
+        m_activeGoal->abort(result);
+    }
+
+    m_activeGoal = goal_handle;
+    std::thread{std::bind(&NavigationComponent::execute, this, std::placeholders::_1), goal_handle}.detach();
+}
+
+void NavigationComponent::execute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<navigation_interfaces::action::GoToPoi>> goal_handle)
+{
+    RCLCPP_INFO(m_node->get_logger(), "GoToPoi Action - Executing goal");
+    rclcpp::Rate loop_rate(1);
+    const auto goal = goal_handle->get_goal();
+    auto feedback = std::make_shared<navigation_interfaces::action::GoToPoi::Feedback>();
+    auto result = std::make_shared<navigation_interfaces::action::GoToPoi::Result>();
+    yarp::dev::Nav2D::NavigationStatusEnum status;
+    {
+        std::lock_guard<std::mutex> lock(m_goalMutex);
+        if (goal_handle != m_activeGoal) {
+            RCLCPP_INFO(m_node->get_logger(), "This goal is preempted %s", goal->poi_name.c_str());
+            return;
+        }
+    }
+
+    if(!m_iNav2D->gotoTargetByLocationName(goal->poi_name))
+    {
+        result->is_ok = false;
+        result->error_msg = "failed to send goal";
+        goal_handle->abort(result);
+        RCLCPP_INFO(m_node->get_logger(), "Goal aborted");
+        return;
+    }
+    do{
+        {
+            std::lock_guard<std::mutex> lock(m_goalMutex);
+            if (goal_handle != m_activeGoal) {
+                RCLCPP_INFO(m_node->get_logger(), "This goal is preempted %s", goal->poi_name.c_str());
+                return;
+            }
+        }
+        // Check if there is a cancel request
+        if (goal_handle->is_canceling()) {
+            result->is_ok = false;
+            result->error_msg = "goal canceled";
+            goal_handle->canceled(result);
+            if(!m_iNav2D->stopNavigation())
+            {
+                RCLCPP_INFO(m_node->get_logger(), "Stop navigation failed");
+            } else 
+            {
+                RCLCPP_INFO(m_node->get_logger(), "Stop navigation");
+            }
+            RCLCPP_INFO(m_node->get_logger(), "Goal canceled");
+            std::lock_guard<std::mutex> lock(m_goalMutex);
+            m_activeGoal = nullptr;
+            return;
+        }
+        
+        if(convertStatus(status).status == navigation_interfaces::msg::NavigationStatus::NAVIGATION_STATUS_ABORTED || 
+            convertStatus(status).status == navigation_interfaces::msg::NavigationStatus::NAVIGATION_STATUS_IDLE)
+        {
+            if(!m_iNav2D->gotoTargetByLocationName(goal->poi_name))
+            {
+                result->is_ok = false;
+                result->error_msg = "failed to send goal";
+                goal_handle->abort(result);
+                RCLCPP_INFO(m_node->get_logger(), "Goal aborted");
+                return;
+            }
+        }
+        if(!m_iNav2D->getNavigationStatus(status))
+        {
+            result->is_ok = false;
+            result->error_msg = "failed to get status";
+            return;
+        }
+        else
+        {
+            feedback->status.status = convertStatus(status).status;
+        }
+        goal_handle->publish_feedback(feedback);
+        RCLCPP_INFO(m_node->get_logger(), "Publish feedback, poi= %s", goal->poi_name.c_str());
+        loop_rate.sleep();
+    }
+    while(rclcpp::ok() && convertStatus(status).status != navigation_interfaces::msg::NavigationStatus::NAVIGATION_STATUS_GOAL_REACHED);
+    // Check if goal is done
+
+    if (rclcpp::ok()) {
+        result->is_ok = true;
+        goal_handle->succeed(result);
+        RCLCPP_INFO(m_node->get_logger(), "Goal succeeded");
+        std::lock_guard<std::mutex> lock(m_goalMutex);
+        m_activeGoal = nullptr;
+    }
+
 }
