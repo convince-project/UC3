ffdd0e148381dc137ed3f3ed6ad4c4370092c737
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-09-24 10:29:26 +0200

    updated notify user component
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- laboratory-tour/src/components/notify_user_component/include/NotifyUserComponent.h
+++ laboratory-tour/src/components/notify_user_component/include/NotifyUserComponent.h
@@ -7,8 +7,8 @@
 
 #include <mutex>
 #include <rclcpp/rclcpp.hpp>
-#include <alarm_interfaces/srv/start_alarm.hpp>
-#include <alarm_interfaces/srv/stop_alarm.hpp>
+#include <notify_user_interfaces/srv/start_alarm.hpp>
+#include <notify_user_interfaces/srv/stop_alarm.hpp>
 
 class NotifyUserComponent
 {
@@ -20,15 +20,15 @@ public:
     bool close();
     void spin();
     static void Alarm(std::shared_ptr<std::atomic<bool>> alarmActive);
-    void StopAlarm([[maybe_unused]] const std::shared_ptr<alarm_interfaces::srv::StopAlarm::Request> request,
-                std::shared_ptr<alarm_interfaces::srv::StopAlarm::Response>      response);
-    void StartAlarm( [[maybe_unused]] const std::shared_ptr<alarm_interfaces::srv::StartAlarm::Request> request,
-                std::shared_ptr<alarm_interfaces::srv::StartAlarm::Response>      response);
+    void StopAlarm([[maybe_unused]] const std::shared_ptr<notify_user_interfaces::srv::StopAlarm::Request> request,
+                std::shared_ptr<notify_user_interfaces::srv::StopAlarm::Response>      response);
+    void StartAlarm( [[maybe_unused]] const std::shared_ptr<notify_user_interfaces::srv::StartAlarm::Request> request,
+                std::shared_ptr<notify_user_interfaces::srv::StartAlarm::Response>      response);
 
 private:
     rclcpp::Node::SharedPtr m_node;
-    rclcpp::Service<alarm_interfaces::srv::StopAlarm>::SharedPtr m_stopAlarmService;
-    rclcpp::Service<alarm_interfaces::srv::StartAlarm>::SharedPtr m_startAlarmService;
+    rclcpp::Service<notify_user_interfaces::srv::StopAlarm>::SharedPtr m_stopAlarmService;
+    rclcpp::Service<notify_user_interfaces::srv::StartAlarm>::SharedPtr m_startAlarmService;
     std::mutex m_mutex;
     std::shared_ptr<std::thread> m_threadAlarm;
     std::shared_ptr<std::atomic<bool>> m_alarmActive;
