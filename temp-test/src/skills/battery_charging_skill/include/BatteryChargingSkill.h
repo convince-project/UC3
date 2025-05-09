dc42e35fb9dcf45a82ad8ae209c0ab010e0dd52a
Beniamino Squitieri <bennibeniamino@gmail.com>
2025-05-09 17:07:06 +0000

    Other high-level skills written but they have not been tested yet with model2code
    Signed-off-by: Beniamino Squitieri <bennibeniamino@gmail.com>

---
+++ temp-test/src/skills/battery_charging_skill/include/BatteryChargingSkill.h
@@ -0,0 +1,59 @@
+# pragma once
+
+#include <mutex>
+#include <thread>
+#include <rclcpp/rclcpp.hpp>
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "BatteryChargingSkillSM.h"
+#include <bt_interfaces_dummy/msg/condition_response.hpp>
+#include <sensor_msgs/msg/battery_state.hpp> 
+
+
+
+#include <bt_interfaces_dummy/srv/tick_condition.hpp>
+
+
+
+#define SERVICE_TIMEOUT 8
+#define SKILL_SUCCESS 0
+#define SKILL_FAILURE 1
+#define SKILL_RUNNING 2
+
+enum class Status{
+	undefined,
+	success,
+	failure
+};
+
+class BatteryChargingSkill
+{
+public:
+	BatteryChargingSkill(std::string name );
+	bool start(int argc, char * argv[]);
+	static void spin(std::shared_ptr<rclcpp::Node> node);
+	
+	void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces_dummy::srv::TickCondition::Request> request,
+			   std::shared_ptr<bt_interfaces_dummy::srv::TickCondition::Response>      response);
+	
+	void topic_callback_readBatteryStatus(const sensor_msgs::msg::BatteryState::SharedPtr msg);
+	
+
+private:
+	std::shared_ptr<std::thread> m_threadSpin;
+	std::shared_ptr<rclcpp::Node> m_node;
+	std::mutex m_requestMutex;
+	std::string m_name;
+	BatteryChargingSkillCondition m_stateMachine;
+	std::atomic<Status> m_tickResult{Status::undefined};
+	rclcpp::Service<bt_interfaces_dummy::srv::TickCondition>::SharedPtr m_tickService;
+	
+	
+	
+	
+	rclcpp::Subscription<sensor_msgs::msg::BatteryState>::SharedPtr m_subscription_readBatteryStatus;
+	
+	
+	
+
+};
+
