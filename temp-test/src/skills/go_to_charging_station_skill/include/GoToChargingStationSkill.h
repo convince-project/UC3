a92bf5472e191940d33f0b6f0c13fb1fdb091315
Beniamino Squitieri <bennibeniamino@gmail.com>
2025-05-15 16:10:55 +0000

    started to fix low level skill pt1
    Signed-off-by: Beniamino Squitieri <bennibeniamino@gmail.com>

---
+++ temp-test/src/skills/go_to_charging_station_skill/include/GoToChargingStationSkill.h
@@ -0,0 +1,64 @@
+# pragma once
+
+#include <mutex>
+#include <thread>
+#include <rclcpp/rclcpp.hpp>
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "GoToChargingStationSkillSM.h"
+#include <bt_interfaces_dummy/msg/action_response.hpp>
+#include <navigation_interfaces/srv/stop_navigation.hpp> 
+#include <navigation_interfaces/srv/go_to_poi_by_name.hpp> 
+#include <navigation_interfaces/srv/get_navigation_status.hpp> 
+#include <navigation_interfaces/srv/check_near_to_poi.hpp> 
+
+
+
+#include <bt_interfaces_dummy/srv/tick_action.hpp>
+#include <bt_interfaces_dummy/srv/halt_action.hpp>
+
+
+#define SERVICE_TIMEOUT 8
+#define SKILL_SUCCESS 0
+#define SKILL_FAILURE 1
+#define SKILL_RUNNING 2
+
+enum class Status{
+	undefined,
+	running, 
+	success,
+	failure
+};
+
+class GoToChargingStationSkill
+{
+public:
+	GoToChargingStationSkill(std::string name );
+	bool start(int argc, char * argv[]);
+	static void spin(std::shared_ptr<rclcpp::Node> node);
+	
+	void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces_dummy::srv::TickAction::Request> request,
+			   std::shared_ptr<bt_interfaces_dummy::srv::TickAction::Response>      response);
+	
+	void halt( [[maybe_unused]] const std::shared_ptr<bt_interfaces_dummy::srv::HaltAction::Request> request,
+			   [[maybe_unused]] std::shared_ptr<bt_interfaces_dummy::srv::HaltAction::Response> response);
+	
+	
+
+private:
+	std::shared_ptr<std::thread> m_threadSpin;
+	std::shared_ptr<rclcpp::Node> m_node;
+	std::mutex m_requestMutex;
+	std::string m_name;
+	GoToChargingStationSkillAction m_stateMachine;
+	std::atomic<Status> m_tickResult{Status::undefined};
+	rclcpp::Service<bt_interfaces_dummy::srv::TickAction>::SharedPtr m_tickService;
+	std::atomic<bool> m_haltResult{false};
+	rclcpp::Service<bt_interfaces_dummy::srv::HaltAction>::SharedPtr m_haltService;
+	
+	
+	
+	
+	
+
+};
+
