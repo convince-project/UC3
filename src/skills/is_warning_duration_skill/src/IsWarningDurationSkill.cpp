f00801fa7b24c056bb07bac466a4bfe7e32eb732
Beniamino Squitieri <bennibeniamino@gmail.com>
2025-05-26 16:00:37 +0000

    modified skills again
    Signed-off-by: Beniamino Squitieri <bennibeniamino@gmail.com>

--- src/skills/is_warning_duration_skill/src/IsWarningDurationSkill.cpp
+++ src/skills/is_warning_duration_skill/src/IsWarningDurationSkill.cpp
@@ -159,7 +159,11 @@ void IsWarningDurationSkill::tick( [[maybe_unused]] const std::shared_ptr<bt_int
           break;
       case Status::success:
           response->status = SKILL_SUCCESS;
-          break;            
+          break;        
+        case Status::undefined:
+          response->status = SKILL_FAILURE;
+          break;    
+        
   }
   RCLCPP_INFO(m_node->get_logger(), "IsWarningDurationSkill::tickDone");
   response->is_ok = true;
