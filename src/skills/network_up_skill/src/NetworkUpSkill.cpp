c052a5752ea4db3d3ba7c934a4b923414a5d5e32
Beniamino Squitieri <bennibeniamino@gmail.com>
2025-05-26 16:08:25 +0000

    modified skills again
    Signed-off-by: Beniamino Squitieri <bennibeniamino@gmail.com>

--- src/skills/network_up_skill/src/NetworkUpSkill.cpp
+++ src/skills/network_up_skill/src/NetworkUpSkill.cpp
@@ -112,7 +112,11 @@ void NetworkUpSkill::tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces_
           break;
       case Status::success:
           response->status = SKILL_SUCCESS;
-          break;            
+          break;           
+        case Status::undefined:      
+          response->status = SKILL_FAILURE;
+          RCLCPP_ERROR(m_node->get_logger(), "NetworkUpSkill::tick - Status is undefined, returning failure.");
+          break; 
   }
   RCLCPP_INFO(m_node->get_logger(), "NetworkUpSkill::tickDone");
   response->is_ok = true;
