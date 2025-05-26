5ec18a33159152b3e9ed7b156a0eca0a666c8520
Beniamino Squitieri <bennibeniamino@gmail.com>
2025-05-26 15:14:30 +0000

    modified again high level skills
    Signed-off-by: Beniamino Squitieri <bennibeniamino@gmail.com>

--- src/skills/is_checking_for_people_skill/src/IsCheckingForPeopleSkill.cpp
+++ src/skills/is_checking_for_people_skill/src/IsCheckingForPeopleSkill.cpp
@@ -69,8 +69,8 @@ bool IsCheckingForPeopleSkill::start(int argc, char*argv[])
   
   m_stateMachine.connectToEvent("BlackboardComponent.GetInt.Call", [this]([[maybe_unused]]const QScxmlEvent & event){
       std::shared_ptr<rclcpp::Node> nodeGetInt = rclcpp::Node::make_shared(m_name + "SkillNodeGetInt");
-      std::shared_ptr<rclcpp::Client<blackboard_interfaces::srv::GetInt>> clientGetInt = nodeGetInt->create_client<blackboard_interfaces::srv::GetInt>("/BlackboardComponent/GetInt");
-      auto request = std::make_shared<blackboard_interfaces::srv::GetInt::Request>();
+      std::shared_ptr<rclcpp::Client<blackboard_interfaces::srv::GetIntBlackboard>> clientGetInt = nodeGetInt->create_client<blackboard_interfaces::srv::GetIntBlackboard>("/BlackboardComponent/GetInt");
+      auto request = std::make_shared<blackboard_interfaces::srv::GetIntBlackboard::Request>();
       auto eventParams = event.data().toMap();
       
       request->field_name = convert<decltype(request->field_name)>(eventParams["field_name"].toString().toStdString());
@@ -160,6 +160,9 @@ void IsCheckingForPeopleSkill::tick( [[maybe_unused]] const std::shared_ptr<bt_i
       case Status::success:
           response->status = SKILL_SUCCESS;
           break;            
+      case Status::undefined:   
+          response->status = SKILL_FAILURE;
+          break;
   }
   RCLCPP_INFO(m_node->get_logger(), "IsCheckingForPeopleSkill::tickDone");
   response->is_ok = true;
