c7ef7a3828fd423ca6f24b7ce3fa2ae648d7015f
Beniamino Squitieri <bennibeniamino@gmail.com>
2025-05-27 11:31:16 +0000

    .
    Signed-off-by: Beniamino Squitieri <bennibeniamino@gmail.com>

--- src/skills/set_navigation_position_skill/src/SetNavigationPositionSkill.cpp
+++ src/skills/set_navigation_position_skill/src/SetNavigationPositionSkill.cpp
@@ -219,7 +219,10 @@ void SetNavigationPositionSkill::tick( [[maybe_unused]] const std::shared_ptr<bt
           break;
       case Status::success:
           response->status = SKILL_SUCCESS;
-          break;            
+          break;  
+      case Status::undefined:
+          response->status = SKILL_FAILURE;
+          break;          
   }
   RCLCPP_INFO(m_node->get_logger(), "SetNavigationPositionSkill::tickDone");
   response->is_ok = true;
