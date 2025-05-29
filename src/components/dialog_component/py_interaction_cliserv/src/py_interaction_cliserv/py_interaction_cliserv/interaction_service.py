43b86236b1e1c226228a01f492b4c08479b2dcec
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2025-05-29 12:53:17 +0200

    added dialog from leonardo
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

---
+++ src/components/dialog_component/py_interaction_cliserv/src/py_interaction_cliserv/py_interaction_cliserv/interaction_service.py
@@ -0,0 +1,106 @@
+import rclpy
+from rclpy.node import Node
+from dialog_interfaces.srv import RememberInteractions
+import time
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
+class InteractionService(Node):
+
+    def __init__(self):
+        super().__init__('py_dialog_component')
+        self.srv = self.create_service(RememberInteractions, '/DialogComponent/RememberInteractions', self.get_previous_similar_interaction)
+
+        self.saved_interactions = dict()
+        self.saved_embeddings = dict()
+
+        start = time.time()
+
+        # dict of sentence models and their thresholds
+        sentence_model_thresholds = {
+            "sentence-transformers/all-MiniLM-L6-v2": 0.5,
+            "sentence-transformers/distiluse-base-multilingual-cased-v1": 1, # needs to be tuned
+            "intfloat/multilingual-e5-large-instruct": 0.925,
+            "paraphrase-multilingual-MiniLM-L12-v2": 0.8
+        }
+
+        # TODO: make it a parameter
+        model_name = "paraphrase-multilingual-MiniLM-L12-v2"
+
+        self.model = SentenceTransformer(model_name)
+
+        self.threshold = sentence_model_thresholds[model_name]
+        self.get_logger().info(f"Model {model_name} loaded with threshold {self.threshold}")
+        self.get_logger().info(f"Model loaded in {time.time() - start} seconds")
+
+        self.get_logger().info('Ready to compare interactions!')
+
+    # return the index of a previous interaction that is similar to the current one if found, otherwise -1
+    def get_previous_similar_interaction(self, request, response):
+
+        if request.is_beginning_of_conversation:
+            self.saved_interactions = dict()
+            self.saved_embeddings = dict()
+            self.get_logger().info("New conversation started, resetting saved interactions and embeddings")
+
+        if request.context not in self.saved_interactions and request.context not in self.saved_embeddings:
+            self.saved_interactions[request.context] = []
+            self.saved_embeddings[request.context] = None
+
+        # self.get_logger().info("Previous interactions: ", self.saved_interactions)
+        self.get_logger().info(f"Current interaction: {request.interaction}")
+
+        # start = time.time()
+        embedding = self.model.encode([request.interaction])
+        # self.get_logger().info("Sentence encoded in %s seconds" % (time.time() - start))
+        # self.get_logger().info(embedding.shape)
+
+        # start = time.time()
+        similarities = self.model.similarity(embedding, self.saved_embeddings[request.context]) if self.saved_embeddings[request.context] is not None else np.array([[0]])
+        # self.get_logger().info("Similarities calculated in %s seconds" % (time.time() - start))
+        # self.get_logger().info(similarities)
+        # self.get_logger().info(similarities.shape)
+
+        max_sim_index = np.unravel_index(np.argmax(similarities, axis=None), similarities.shape)
+        max_sim_value = similarities[max_sim_index].item()
+        # self.get_logger().info(max_sim_value, max_sim_index)
+
+        if max_sim_value > self.threshold:
+            # self.get_logger().info(self.saved_interactions)
+            # self.get_logger().info(similarities)
+            # TODO: Discuss if skipping the embedding is a good idea, or if it's better computing the average of the duplicate embeddings
+            duplicate_index = int(max_sim_index[1])
+        else:
+            # start = time.time()
+            self.saved_interactions[request.context].append(request.interaction)
+            self.saved_embeddings[request.context] = np.concatenate(
+                (self.saved_embeddings[request.context], embedding)
+            ) if self.saved_embeddings[request.context] is not None else embedding
+            # self.get_logger().info("Embeddings concatenated in %s seconds" % (time.time() - start))
+            # self.get_logger().info(self.saved_embeddings.shape)
+
+            duplicate_index = -1
+        
+        # self.response.data = f"{self.saved_interactions[self.duplicate_index]}"
+        response.index = duplicate_index
+        response.is_ok = True
+        if duplicate_index != -1:
+            self.get_logger().info(f'Duplicate interaction detected: previous "{self.saved_interactions[request.context][duplicate_index]}" and current "{request.interaction}"')
+
+        self.get_logger().info("-" * 50)
+
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    interaction_service = InteractionService()
+
+    rclpy.spin(interaction_service)
+
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- src/skills/dialog_skill/include/DialogSkill.h
+++ src/skills/dialog_skill/include/DialogSkill.h
@@ -1,30 +1,35 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2020 Fondazione Istituto Italiano di Tecnologia (IIT)        *
+ * All Rights Reserved.                                                       *
+ *                                                                            *
+ ******************************************************************************/
+
 # pragma once
 
 #include <mutex>
 #include <thread>
 #include <rclcpp/rclcpp.hpp>
-#include "rclcpp_action/rclcpp_action.hpp"
 #include "DialogSkillSM.h"
-#include <bt_interfaces_dummy/msg/action_response.hpp>
-#include <scheduler_interfaces/srv/get_current_language.hpp> 
-#include <dialog_interfaces/srv/set_language.hpp> 
-#include <dialog_interfaces/srv/get_state.hpp> 
-#include <dialog_interfaces/srv/enable_dialog.hpp> 
+#include <bt_interfaces/msg/action_response.hpp>
+#include <scheduler_interfaces/srv/get_current_language.hpp>
 
+#include <bt_interfaces/srv/tick_action.hpp>
+#include <bt_interfaces/srv/halt_action.hpp>
 
+#include <dialog_interfaces/srv/manage_context.hpp>
+#include <dialog_interfaces/srv/remember_interactions.hpp> // auto-generated from the .srv file
+#include <dialog_interfaces/srv/wait_for_interaction.hpp>
+#include <dialog_interfaces/srv/shorten_and_speak.hpp>
+#include <dialog_interfaces/srv/answer_and_speak.hpp>
+#include <dialog_interfaces/srv/set_language.hpp>
+#include <dialog_interfaces/srv/interpret_command.hpp>
 
-#include <bt_interfaces_dummy/srv/tick_action.hpp>
-#include <bt_interfaces_dummy/srv/halt_action.hpp>
-
-
-#define SERVICE_TIMEOUT 8
-#define SKILL_SUCCESS 0
-#define SKILL_FAILURE 1
-#define SKILL_RUNNING 2
+#include <text_to_speech_interfaces/srv/set_microphone.hpp>
 
 enum class Status{
 	undefined,
-	running, 
+	running,
 	success,
 	failure
 };
@@ -35,30 +40,26 @@ public:
 	DialogSkill(std::string name );
 	bool start(int argc, char * argv[]);
 	static void spin(std::shared_ptr<rclcpp::Node> node);
-	
-	void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces_dummy::srv::TickAction::Request> request,
-			   std::shared_ptr<bt_interfaces_dummy::srv::TickAction::Response>      response);
-	
-	void halt( [[maybe_unused]] const std::shared_ptr<bt_interfaces_dummy::srv::HaltAction::Request> request,
-			   [[maybe_unused]] std::shared_ptr<bt_interfaces_dummy::srv::HaltAction::Response> response);
-	
-	
+	void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces::srv::TickAction::Request> request,
+			   std::shared_ptr<bt_interfaces::srv::TickAction::Response>      response);
+	void halt( [[maybe_unused]] const std::shared_ptr<bt_interfaces::srv::HaltAction::Request> request,
+			   [[maybe_unused]] std::shared_ptr<bt_interfaces::srv::HaltAction::Response> response);
 
 private:
+	void EnableMicrophone();
+    void DisableMicrophone();
+
+
 	std::shared_ptr<std::thread> m_threadSpin;
 	std::shared_ptr<rclcpp::Node> m_node;
 	std::mutex m_requestMutex;
 	std::string m_name;
 	DialogSkillAction m_stateMachine;
+	
 	std::atomic<Status> m_tickResult{Status::undefined};
-	rclcpp::Service<bt_interfaces_dummy::srv::TickAction>::SharedPtr m_tickService;
+	rclcpp::Service<bt_interfaces::srv::TickAction>::SharedPtr m_tickService;
 	std::atomic<bool> m_haltResult{false};
-	rclcpp::Service<bt_interfaces_dummy::srv::HaltAction>::SharedPtr m_haltService;
+	rclcpp::Service<bt_interfaces::srv::HaltAction>::SharedPtr m_haltService;
 	
-	
-	
-	
-	
-
 };
 
