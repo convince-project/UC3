414b0f450defc868144d217f3bb9db891e23d518
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2025-05-29 14:14:43 +0200

    added bt interfaces dummy instead of bt_interfaces
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- src/skills/dialog_skill/include/DialogSkill.h
+++ src/skills/dialog_skill/include/DialogSkill.h
@@ -40,10 +40,10 @@ public:
 	DialogSkill(std::string name );
 	bool start(int argc, char * argv[]);
 	static void spin(std::shared_ptr<rclcpp::Node> node);
-	void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces::srv::TickAction::Request> request,
-			   std::shared_ptr<bt_interfaces::srv::TickAction::Response>      response);
-	void halt( [[maybe_unused]] const std::shared_ptr<bt_interfaces::srv::HaltAction::Request> request,
-			   [[maybe_unused]] std::shared_ptr<bt_interfaces::srv::HaltAction::Response> response);
+	void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces_dummy::srv::TickAction::Request> request,
+			   std::shared_ptr<bt_interfaces_dummy::srv::TickAction::Response>      response);
+	void halt( [[maybe_unused]] const std::shared_ptr<bt_interfaces_dummy::srv::HaltAction::Request> request,
+			   [[maybe_unused]] std::shared_ptr<bt_interfaces_dummy::srv::HaltAction::Response> response);
 
 private:
 	void EnableMicrophone();
@@ -57,9 +57,9 @@ private:
 	DialogSkillAction m_stateMachine;
 	
 	std::atomic<Status> m_tickResult{Status::undefined};
-	rclcpp::Service<bt_interfaces::srv::TickAction>::SharedPtr m_tickService;
+	rclcpp::Service<bt_interfaces_dummy::srv::TickAction>::SharedPtr m_tickService;
 	std::atomic<bool> m_haltResult{false};
-	rclcpp::Service<bt_interfaces::srv::HaltAction>::SharedPtr m_haltService;
+	rclcpp::Service<bt_interfaces_dummy::srv::HaltAction>::SharedPtr m_haltService;
 	
 };
 
