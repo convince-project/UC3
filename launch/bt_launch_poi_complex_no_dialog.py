a18bdd6c09035f431997c720c826243e11ec076e
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2025-05-30 10:00:42 +0200

    added bt no dialog
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

---
+++ launch/bt_launch_poi_complex_no_dialog.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='bt_executable_reload',
+            executable='bt_executable_reload',
+            arguments=[ './src/behavior_tree/BT/bt_poi_complex_no_dialog.xml']
+        ), 
+    ])
+
