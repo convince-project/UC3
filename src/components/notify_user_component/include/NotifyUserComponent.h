c80d43955b8db607289cbe4b3f808dcae2d15fc5
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2025-05-30 09:39:13 +0200

    removed alarm_interfaces and alarm skill
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- src/components/notify_user_component/include/NotifyUserComponent.h
+++ src/components/notify_user_component/include/NotifyUserComponent.h
@@ -7,8 +7,8 @@
 
 #include <mutex>
 #include <rclcpp/rclcpp.hpp>
-#include <alarm_interfaces/srv/start_alarm.hpp>
-#include <alarm_interfaces/srv/stop_alarm.hpp>
+#include <notify_user_interfaces/srv/start_alarm.hpp>
+#include <notify_user_interfaces/srv/stop_alarm.hpp>
 #include <notify_user_interfaces/srv/notify_user_charged.hpp>
 
 class NotifyUserComponent
@@ -21,17 +21,17 @@ public:
     bool close();
     void spin();
     static void Alarm(std::shared_ptr<std::atomic<bool>> alarmActive);
-    void StopAlarm([[maybe_unused]] const std::shared_ptr<alarm_interfaces::srv::StopAlarm::Request> request,
-                std::shared_ptr<alarm_interfaces::srv::StopAlarm::Response>      response);
-    void StartAlarm( [[maybe_unused]] const std::shared_ptr<alarm_interfaces::srv::StartAlarm::Request> request,
-                std::shared_ptr<alarm_interfaces::srv::StartAlarm::Response>      response);
+    void StopAlarm([[maybe_unused]] const std::shared_ptr<notify_user_interfaces::srv::StopAlarm::Request> request,
+                std::shared_ptr<notify_user_interfaces::srv::StopAlarm::Response>      response);
+    void StartAlarm( [[maybe_unused]] const std::shared_ptr<notify_user_interfaces::srv::StartAlarm::Request> request,
+                std::shared_ptr<notify_user_interfaces::srv::StartAlarm::Response>      response);
     void NotifyUserCharged( [[maybe_unused]] const std::shared_ptr<notify_user_interfaces::srv::NotifyUserCharged::Request> request,
                 std::shared_ptr<notify_user_interfaces::srv::NotifyUserCharged::Response>      response);
 
 private:
     rclcpp::Node::SharedPtr m_node;
-    rclcpp::Service<alarm_interfaces::srv::StopAlarm>::SharedPtr m_stopAlarmService;
-    rclcpp::Service<alarm_interfaces::srv::StartAlarm>::SharedPtr m_startAlarmService;
+    rclcpp::Service<notify_user_interfaces::srv::StopAlarm>::SharedPtr m_stopAlarmService;
+    rclcpp::Service<notify_user_interfaces::srv::StartAlarm>::SharedPtr m_startAlarmService;
     rclcpp::Service<notify_user_interfaces::srv::NotifyUserCharged>::SharedPtr m_notifyUserChargedService;
     std::mutex m_mutex;
     std::shared_ptr<std::thread> m_threadAlarm;
