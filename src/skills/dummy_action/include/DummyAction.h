3002a14695fae261d902236b100c84e7c9d4b749
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2025-05-30 09:59:56 +0200

    added dummy condition and action
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

---
+++ src/skills/dummy_action/include/DummyAction.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2020 Fondazione Istituto Italiano di Tecnologia (IIT)        *
+ * All Rights Reserved.                                                       *
+ *                                                                            *
+ ******************************************************************************/
+
+# pragma once
+
+#include <thread>
+#include <rclcpp/rclcpp.hpp>
+#include <bt_interfaces_dummy/msg/action_response.hpp>
+#include <bt_interfaces_dummy/srv/tick_action.hpp>
+#include <bt_interfaces_dummy/srv/halt_action.hpp>
+#include <mutex>
+
+#include <yarp/os/Network.h>
+#include <yarp/os/Port.h>
+#include <yarp/os/Bottle.h>
+#include <yarp/os/RpcServer.h>
+#include <yarp/os/BufferedPort.h>
+
+class DummyAction :
+    public yarp::os::PortReader,
+    public rclcpp::Node
+{
+public:
+    DummyAction(std::string name, std::string default_status );
+    ~DummyAction();
+
+    void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces_dummy::srv::TickAction::Request> request,
+               std::shared_ptr<bt_interfaces_dummy::srv::TickAction::Response> response);
+    void halt( [[maybe_unused]] const std::shared_ptr<bt_interfaces_dummy::srv::HaltAction::Request> request,
+               [[maybe_unused]] std::shared_ptr<bt_interfaces_dummy::srv::HaltAction::Response> response);
+    bool read(yarp::os::ConnectionReader& connection) override;
+
+private:
+    uint8_t          m_status;
+    std::string      m_name;
+    std::mutex       m_mutex;
+    yarp::os::Port   m_changeStatusPort;
+    rclcpp::Service<bt_interfaces_dummy::srv::TickAction>::SharedPtr m_tickService;
+    rclcpp::Service<bt_interfaces_dummy::srv::HaltAction>::SharedPtr m_haltService;
+
+};
