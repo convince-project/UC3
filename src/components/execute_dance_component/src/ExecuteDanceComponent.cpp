5d12e3b5d5d5063fad8787f94efde4379b650645
Leonardo Gori <leonardo.gori@iit.it>
2025-07-16 16:07:35 +0000

    Moved dance execution from ctpService to yarpActionsPlayer

--- src/components/execute_dance_component/src/ExecuteDanceComponent.cpp
+++ src/components/execute_dance_component/src/ExecuteDanceComponent.cpp
@@ -49,6 +49,16 @@ bool ExecuteDanceComponent::start(int argc, char*argv[])
     {
         yWarning() << "Movement part names are empty. No movements will be executed!";
     }
+
+    yAPClientPortName = "/ExecuteDanceComponent/yarpActionsPlayerClient/rpc";
+    bool b = m_yAPClientPort.open(yAPClientPortName);
+    if (!b)
+    {
+        yError() << "Cannot open yarpActionsPlayer client port";
+        return false;
+    }
+    yarp::os::Network::connect(yAPClientPortName, "/yarpActionsPlayer/rpc");
+
     
     m_node = rclcpp::Node::make_shared("ExecuteDanceComponentNode");
     m_executeDanceService = m_node->create_service<execute_dance_interfaces::srv::ExecuteDance>("/ExecuteDanceComponent/ExecuteDance",  
@@ -110,57 +120,16 @@ void ExecuteDanceComponent::executeTask(const std::shared_ptr<execute_dance_inte
         }
         m_threadTimer = std::thread([this, danceDuration]() { timerTask(danceDuration->duration); });
     }
-    do {
-        //calls the GetMovement service
-        auto getMovementClientNode = rclcpp::Node::make_shared("ExecuteDanceComponentGetMovementNode");
-        std::shared_ptr<rclcpp::Client<dance_interfaces::srv::GetMovement>> getMovementClient =
-        getMovementClientNode->create_client<dance_interfaces::srv::GetMovement>("/DanceComponent/GetMovement");
-        auto getMovementRequest = std::make_shared<dance_interfaces::srv::GetMovement::Request>();
-        while(!getMovementClient->wait_for_service(std::chrono::seconds(1))) {
-            if (!rclcpp::ok()) {
-                RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service 'getMovementClient'. Exiting.");
-            }
-        }
-        auto getMovementResult = getMovementClient->async_send_request(getMovementRequest);
-        auto futureGetMovementResult = rclcpp::spin_until_future_complete(getMovementClientNode, getMovementResult);
-        auto movement = getMovementResult.get();
-        if (movement->is_ok == false) {
-            RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "ExecuteDanceComponent::ExecuteDance. Movement not found, skipping...");
-        } else {
-            bool status;
-            if(m_danceName == "idleMove" || m_danceName == "navigationPosition")
-            {
-                status = SendMovementNow(movement->time, movement->offset, movement->joints, m_pCtpService.at(movement->part_name));
+    
+    bool status;
 
-            }
-            else{
-                status = SendMovementToQueue(movement->time, movement->offset, movement->joints, m_pCtpService.at(movement->part_name));
-            }
-            if (!status)
-            {
-                RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Movement failed to sent. Is ctpService for part" << movement->part_name << "running?");
-                continue;
-            }            
-        }
-        //calls the UpdateMovement service
-        auto updateMovementClientNode = rclcpp::Node::make_shared("ExecuteDanceComponentUpdateMovementNode");
-        std::shared_ptr<rclcpp::Client<dance_interfaces::srv::UpdateMovement>> updateMovementClient =
-        updateMovementClientNode->create_client<dance_interfaces::srv::UpdateMovement>("/DanceComponent/UpdateMovement");
-        auto updateMovementRequest = std::make_shared<dance_interfaces::srv::UpdateMovement::Request>();
-        while(!updateMovementClient->wait_for_service(std::chrono::seconds(1))) {
-            if (!rclcpp::ok()) {
-                RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service 'updateMovementClient'. Exiting.");
-            }
-        }
-        auto updateMovementResult = updateMovementClient->async_send_request(updateMovementRequest);
-        auto futureUpdateMovementResult = rclcpp::spin_until_future_complete(updateMovementClientNode, updateMovementResult);
-        auto updateMovementResponse = updateMovementResult.get();
-        if (updateMovementResponse->is_ok == false) {
-            RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "ExecuteDanceComponent::ExecuteDance. Movement not found, skipping...");
-        }
+    status = SendMovementToYAP(request->dance_name);
+    if (!status)
+    {
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Movement failed to sent to YAP");
+        return;
+    }
 
-        done_with_getting_dance = updateMovementResponse->done_with_dance;
-    } while (!done_with_getting_dance);  
     RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Done getting Dance");
 }
 
@@ -255,4 +224,49 @@ bool ExecuteDanceComponent::SendMovementNow(float time, int offset, std::vector<
         list.addFloat64(joint);
     }
     return port.write(cmd, res);
+}
+
+bool ExecuteDanceComponent::SendMovementToYAP(const std::string &actionName)
+{
+    yarp::os::Bottle res;
+    yarp::os::Bottle cmd;
+    cmd.clear();
+    res.clear();
+    cmd.addString("start");
+    // cmd.addString(actionName);
+
+    std::cout << "ExecuteDanceComponent::SendMovementToYAP sending bottle content: " << cmd.toString() << " and action name is " << actionName << std::endl;
+
+    bool status = m_yAPClientPort.write(cmd, res);
+
+    if (!status)
+    {
+        RCLCPP_ERROR_STREAM(m_node->get_logger(), "ExecuteDanceComponent::SendMovementToYAP Failed to send choose_action command to YAP");
+        return false;
+    }
+
+    if (res.get(0).asVocab32() != yarp::os::createVocab32('a', 'c', 'k'))
+    {
+        RCLCPP_ERROR_STREAM(m_node->get_logger(), "ExecuteDanceComponent::SendMovementToYAP YAP did not accept the action: " << actionName);
+        return false;
+    }
+    else
+    {
+        RCLCPP_INFO_STREAM(m_node->get_logger(), "ExecuteDanceComponent::SendMovementToYAP YAP accepted the action: " << actionName);
+    }
+
+
+    cmd.addString("reset");
+
+    status = m_yAPClientPort.write(cmd, res);
+
+    if (!status)
+    {
+        RCLCPP_ERROR_STREAM(m_node->get_logger(), "ExecuteDanceComponent::SendMovementToYAP Failed to send reset command to YAP");
+        return false;
+    }
+
+    RCLCPP_INFO_STREAM(m_node->get_logger(), "ExecuteDanceComponent::SendMovementToYAP Reset status: " << res.get(0).toString());
+
+    return true;
 }
