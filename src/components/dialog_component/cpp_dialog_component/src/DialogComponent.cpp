4520b870b2e1fa57d74fee68f047a6e4ca27ef4f
Leonardo Gori <leonardo.gori@iit.it>
2025-07-18 16:09:07 +0000

    added execute dance logic into dialog component

--- src/components/dialog_component/cpp_dialog_component/src/DialogComponent.cpp
+++ src/components/dialog_component/cpp_dialog_component/src/DialogComponent.cpp
@@ -1110,7 +1110,7 @@ void DialogComponent::WaitForInteraction(const std::shared_ptr<GoalHandleWaitFor
 
 // WaitForInteraction action fragment of code end
 
-// Protected function to actually speak given the text
+
 void DialogComponent::ExecuteDance(std::string danceName, float estimatedSpeechTime)
 {
 
