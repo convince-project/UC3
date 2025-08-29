0c8a2dd0fad38e4e997c7083c11e1da813faf697
Your Name <you@example.com>
2025-08-29 18:17:00 +0200

    ponting strategy to be checked but the r1-cartesian-controller catches points correctly. An other thing to be checked is if it's possible to change the end effector frame in order to avoid hand orientation
    Signed-off-by: Your Name <you@example.com>

--- src/components/execute_dance_component/include/ExecuteDanceComponent.h
+++ src/components/execute_dance_component/include/ExecuteDanceComponent.h
@@ -134,10 +134,11 @@ private:
 
     // Nomi dei frame (override da config se vuoi)
     std::string m_mapFrame        {"map"};
-    std::string m_baseFrame      {"mobile_base_body_link"}
-    std::string m_torsoFrame     {"chest_link"};
-    std::m_lShoulderFrame {"l_shoulder_1"};
-    std::string m_rShoulderFrame {"r_shoulder_1"};
+    std::string m_baseFrame       {"mobile_base_body_link"};
+    std::string m_torsoFrame      {"chest_link"};
+    std::string m_lShoulderFrame  {"l_shoulder_1"};
+    std::string m_rShoulderFrame  {"r_shoulder_1"};
+
 
     // Parametri di pointing (tarabili)
     double m_reachRadius   {0.60};   // raggio max su cui proiettare il candidato dalla spalla
