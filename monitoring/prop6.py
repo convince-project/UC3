e314eeea5736a55de109fc3dbdf2a7dbad3710f6
fincatomarta <fincatomarta16@gmail.com>
2025-09-17 10:08:20 +0000

    Added fakeGrabber

--- monitoring/prop6.py
+++ monitoring/prop6.py
@@ -16,16 +16,11 @@ def abstract_message(message):
         predicates['time'] += 0.0000001
     else:
         predicates['time'] = message['time']
-    print("message", message)
+    print("camera_received, topic ", message['topic'])
     if message['topic'] in [
-        "camera_left/image_rect_color",
-        "camera_right/image_rect_color",
-        "camera_rgbd/color/image_rect_color",
-        "camera_rgbd/depth/image_rect",
-        "camera/depth/color/points"
+        "camera_out"
     ]:
         predicates['camera_published'] = True
     elif message['topic'] == "clock":
         predicates['camera_published'] = False
-    print("predicates", predicates)
     return predicates
