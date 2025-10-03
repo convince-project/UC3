e6363c73196ca845ae53bf87faafbc50b516bee3
fbrand-new <francesco.brand@iit.it>
2025-10-03 13:20:33 +0200

    fiX: fixed other hl skills after rebase
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- src/skills/people_left_skill/include/PeopleLeftSkill.h
+++ src/skills/people_left_skill/include/PeopleLeftSkill.h
@@ -8,6 +8,7 @@
 #include <bt_interfaces_dummy/msg/action_response.hpp>
 #include <turn_back_manager_interfaces/srv/reset_counters.hpp> 
 #include <time_interfaces/srv/stop_tour_timer.hpp> 
+#include <time_interfaces/srv/stop_poi_timer.hpp> 
 #include <scheduler_interfaces/srv/reset.hpp> 
 
 
