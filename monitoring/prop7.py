8128e4f1425c7e64be3ba1869ea0ca333e8d92c9
fincatomarta <fincatomarta16@gmail.com>
2025-10-07 14:37:57 +0000

    fixed property 7

--- monitoring/prop7.py
+++ monitoring/prop7.py
@@ -2,7 +2,7 @@
 H(POI_1_selected => F[0:t] POI_1_completed)
 """
 # property definition for POI 7 completion within time t
-PROPERTY = r"historically( not( not {po1_completed} since[30:] {poi1_selected}))"
+PROPERTY = r"historically( not( not {po1_completed} since[50:] {poi1_selected}))"
 
 
 # predicates used in the property (initialization for time 0)
@@ -20,60 +20,38 @@ def abstract_message(message):
     else:
         predicates['time'] = message['time']
 
-    print("message", message)
+    # print("message", message)
     print("predicates", predicates)
 
     if message['topic'] == "/monitoring_clock":
         return predicates
     # Global map to match requests and responses through the sequence_number
-    if not hasattr(abstract_message, "pending_requests"):
-        abstract_message.pending_requests = {}
 
     # int8 SKILL_SUCCESS=0
     # int8 SKILL_FAILURE=1
     # int8 SKILL_RUNNING=2
     if "topic" in message and "GetCurrentPoi" in message['topic']:
-        sequence_number = None
-        if "info" in message and isinstance(message["info"], dict):
-            sequence_number = message["info"].get("sequence_number")
-        # If there is a request, save the sequence_number
-        if "request" in message and isinstance(message["request"], list) and sequence_number is not None:
-            abstract_message.pending_requests["poi_selected_" + str(sequence_number)] = True
-        # If there is a response, check if the request matches through sequence_number
-        if "response" in message and isinstance(message["response"], list) and sequence_number is not None:
+        print("message", message)
+        if "response" in message:
             for resp in message["response"]:
                 if resp.get("poi_number") == 1:
-                    key = "poi_selected_" + str(sequence_number)
-                    if abstract_message.pending_requests.get(key):
-                        predicates['poi1_selected'] = True
-                        del abstract_message.pending_requests[key]
+                   predicates['poi1_selected'] = True
+                        
     if "topic" in message and "GetInt" in message['topic']:
         print("in if GetInt")
-        sequence_number = None
-        if "info" in message and isinstance(message["info"], dict):
-            sequence_number = message["info"].get("sequence_number")
-        # If there is a request, save the associated field_name with the sequence_number
-        if "request" in message and isinstance(message["request"], list) and sequence_number is not None:
-            for req in message["request"]:
-                print("in for", req)
-                if req.get("field_name") == "PoiDone1":
-                    print("field name found")
-                    abstract_message.pending_requests[sequence_number] = "PoiDone1"
-        # If there is a response, check if value==0 and the request matches through sequence_number
-        if "response" in message and isinstance(message["response"], list) and sequence_number is not None:
+        if "response" in message:
             for resp in message["response"]:
-                if resp.get("value") == 0:
-                    print("in get value")
-                    field_name = abstract_message.pending_requests.get(sequence_number)
-                    print("field_name", field_name)
-                    if field_name == "PoiDone1":
+                field_name = resp.get("field_name")
+                print("message", message)
+                if field_name == "PoiDone1":
+                    if resp.get("error_msg") == "":
                         predicates['poi1_completed'] = True
-                        # Rimuovi la richiesta accoppiata
-                        del abstract_message.pending_requests[sequence_number]
-
+                        # print("predicates", predicates)
+                    else:
+                        predicates['poi1_completed'] = False
+                    # Rimuovi la richiesta accoppiata
     # predicates['service'] = True if 'service' in message else False
 
-    # predicates['low_percentage'] = True if 'percentage' in message and message['percentage'] < 30 else False
-
+    # predicates['low_percentage'] = True if 'percentage' in message and message['percentage'] < 30 else Fals
 
     return predicates
