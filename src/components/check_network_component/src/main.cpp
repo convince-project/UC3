2408768f7e3aa68d5e14c42628b6c0ee32ede1d1
Ettore Landini <ettore.landini@iit.it>
2025-10-22 15:51:20 +0200

    Web access check working
    To be improved
    
    Signed-off-by: Ettore Landini <ettore.landini@iit.it>

--- src/components/check_network_component/src/main.cpp
+++ src/components/check_network_component/src/main.cpp
@@ -1,20 +1,22 @@
-
-
 #include <iostream>
 #include "CheckNetworkComponent.h"
+#include <yarp/os/Network.h>
 
 #include <thread>
 #include <chrono>
 
 int main(int argc, char *argv[])
 {
-  
-  CheckNetworkComponent component;
-  component.set_name("CheckNetworkComponent");
-  component.setup(argc, argv);
-  component.start();
 
-  return true;
-
+    CheckNetworkComponent component;
+    component.set_name("CheckNetworkComponent");
+    yarp::os::Network yarp(yarp::os::YARP_CLOCK_SYSTEM);
+    std::cout << "YARP configured successfully in main" << std::endl;
+    if (!component.setup(argc, argv)) {
+        RCLCPP_ERROR(component.getLogger(), "[main] Unable to start CheckNetworkComponent");
+        return 1;
+    }
+    component.start();
+    return 0;
 }
 
