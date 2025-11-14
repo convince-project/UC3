a5405a2dbc6ea9c868777225585dff75b8aa0996
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2025-11-14 18:26:45 +0100

    updated hardware_fault_skill
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

---
+++ src/skills/hardware_fault_skill/include/HardwareFaultSkill.h
@@ -0,0 +1,61 @@
+# pragma once
+
+#include <mutex>
+#include <thread>
+#include <rclcpp/rclcpp.hpp>
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "HardwareFaultSkillSM.h"
+#include <bt_interfaces_dummy/msg/condition_response.hpp>
+#include <hardware_monitor_interfaces/srv/has_faults.hpp> 
+
+
+
+#include <bt_interfaces_dummy/srv/tick_condition.hpp>
+
+
+#include <rcl/service_introspection.h>
+
+#define SERVICE_TIMEOUT 8
+#define SKILL_SUCCESS 0
+#define SKILL_FAILURE 1
+#define SKILL_RUNNING 2
+
+enum class Status{
+	undefined,
+	success,
+	failure
+};
+
+class HardwareFaultSkill
+{
+public:
+	HardwareFaultSkill(std::string name );
+    ~HardwareFaultSkill();
+
+	bool start(int argc, char * argv[]);
+	static void spin(std::shared_ptr<rclcpp::Node> node);
+	
+	void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces_dummy::srv::TickCondition::Request> request,
+			   std::shared_ptr<bt_interfaces_dummy::srv::TickCondition::Response>      response);
+	
+	
+	
+
+private:
+	std::shared_ptr<std::thread> m_threadSpin;
+	std::shared_ptr<rclcpp::Node> m_node;
+	std::mutex m_requestMutex;
+	std::string m_name;
+	HardwareFaultSkillCondition m_stateMachine;
+	std::atomic<Status> m_tickResult{Status::undefined};
+	rclcpp::Service<bt_interfaces_dummy::srv::TickCondition>::SharedPtr m_tickService;
+	
+	
+	
+	
+	
+	
+	
+
+};
+
---
+++ src/skills/hardware_fault_skill/src/HardwareFaultSkill.cpp
@@ -0,0 +1,187 @@
+#include "HardwareFaultSkill.h"
+#include <future>
+#include <QTimer>
+#include <QDebug>
+#include <QCoreApplication>
+
+#include <QTime>
+#include <iostream>
+#include <QStateMachine>
+
+#include <type_traits>
+
+template<typename T>
+T convert(const std::string& str) {
+    if constexpr (std::is_same_v<T, int>) {
+        return std::stoi(str);
+    } else if constexpr (std::is_same_v<T, double>) {
+        return std::stod(str);
+    } else if constexpr (std::is_same_v<T, float>) {
+        return std::stof(str);
+    } 
+    else if constexpr (std::is_same_v<T, bool>) { 
+        if (str == "true" || str == "1") { 
+            return true; 
+        } else if (str == "false" || str == "0") { 
+            return false; 
+        } else { 
+            throw std::invalid_argument("Invalid boolean value"); 
+        } 
+    } 
+    else if constexpr (std::is_same_v<T, std::string>) {
+        return str;
+    }
+    else {
+        throw std::invalid_argument("Unsupported type conversion");
+    }
+}
+
+HardwareFaultSkill::HardwareFaultSkill(std::string name ) :
+		m_name(std::move(name))
+{
+    
+}
+
+HardwareFaultSkill::~HardwareFaultSkill()
+{
+    //std::cout << "DEBUG: Invoked destructor of HardwareFaultSkill" << std::endl;
+    m_threadSpin->join();
+}
+
+void HardwareFaultSkill::spin(std::shared_ptr<rclcpp::Node> node)
+{
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    QCoreApplication::quit();
+    //std::cout << "DEBUG: HardwareFaultSkill::spin successfully ended" << std::endl;
+}
+
+bool HardwareFaultSkill::start(int argc, char*argv[])
+{
+	if(!rclcpp::ok())
+	{
+		rclcpp::init(/*argc*/ argc, /*argv*/ argv);
+	}
+
+	m_node = rclcpp::Node::make_shared(m_name + "Skill");
+	RCLCPP_DEBUG_STREAM(m_node->get_logger(), "HardwareFaultSkill::start");
+	std::cout << "DEBUG: HardwareFaultSkill::start" << std::endl;
+
+  
+	m_tickService = m_node->create_service<bt_interfaces_dummy::srv::TickCondition>(m_name + "Skill/tick",
+                                                                           	std::bind(&HardwareFaultSkill::tick,
+                                                                           	this,
+                                                                           	std::placeholders::_1,
+                                                                           	std::placeholders::_2));
+  m_tickService->configure_introspection(m_node->get_clock(), rclcpp::SystemDefaultsQoS(), RCL_SERVICE_INTROSPECTION_CONTENTS);
+  
+  
+  
+  
+  m_stateMachine.connectToEvent("HardwareMonitorComponent.HasFaults.Call", [this]([[maybe_unused]]const QScxmlEvent & event){
+      std::shared_ptr<rclcpp::Node> nodeHasFaults = rclcpp::Node::make_shared(m_name + "SkillNodeHasFaults");
+      std::shared_ptr<rclcpp::Client<hardware_monitor_interfaces::srv::HasFaults>> clientHasFaults = nodeHasFaults->create_client<hardware_monitor_interfaces::srv::HasFaults>("/HardwareMonitorComponent/HasFaults");
+      auto request = std::make_shared<hardware_monitor_interfaces::srv::HasFaults::Request>();
+      clientHasFaults->configure_introspection(nodeHasFaults->get_clock(), rclcpp::SystemDefaultsQoS(), RCL_SERVICE_INTROSPECTION_CONTENTS);
+      auto eventParams = event.data().toMap();
+      
+      bool wait_succeded{true};
+      int retries = 0;
+      while (!clientHasFaults->wait_for_service(std::chrono::seconds(1))) {
+          if (!rclcpp::ok()) {
+              RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service 'HasFaults'. Exiting.");
+              wait_succeded = false;
+              break;
+          } 
+          retries++;
+          if(retries == SERVICE_TIMEOUT) {
+              RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Timed out while waiting for the service 'HasFaults'.");
+              wait_succeded = false;
+              break;
+          }
+      }
+      if (wait_succeded) {                                                                   
+          auto result = clientHasFaults->async_send_request(request);
+          const std::chrono::seconds timeout_duration(SERVICE_TIMEOUT);
+          auto futureResult = rclcpp::spin_until_future_complete(nodeHasFaults, result, timeout_duration);
+          if (futureResult == rclcpp::FutureReturnCode::SUCCESS) 
+          {
+              auto response = result.get();
+              QVariantMap data;
+              data.insert("call_succeeded", true);
+              data.insert("has_fault", response->has_fault);
+              m_stateMachine.submitEvent("HardwareMonitorComponent.HasFaults.Return", data);
+              RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "HardwareMonitorComponent.HasFaults.Return");
+              return;
+              
+          }
+          else if(futureResult == rclcpp::FutureReturnCode::TIMEOUT){
+              RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Timed out while future complete for the service 'HasFaults'.");
+          }
+      }
+      QVariantMap data;
+      data.insert("call_succeeded", false);
+      m_stateMachine.submitEvent("HardwareMonitorComponent.HasFaults.Return", data);
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "HardwareMonitorComponent.HasFaults.Return");
+  });
+  
+  m_stateMachine.connectToEvent("TICK_RESPONSE", [this]([[maybe_unused]]const QScxmlEvent & event){
+    RCLCPP_INFO(m_node->get_logger(), "HardwareFaultSkill::tickReturn %s", event.data().toMap()["status"].toString().toStdString().c_str());
+    std::string result = event.data().toMap()["status"].toString().toStdString();
+    if (result == std::to_string(SKILL_SUCCESS) )
+    {
+      m_tickResult.store(Status::success);
+    }
+    else if (result == std::to_string(SKILL_FAILURE) )
+    { 
+      m_tickResult.store(Status::failure);
+    }
+  });
+    
+
+  
+  
+  
+  
+
+	m_stateMachine.start();
+	m_threadSpin = std::make_shared<std::thread>(spin, m_node);
+       
+	return true;
+}
+
+void HardwareFaultSkill::tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces_dummy::srv::TickCondition::Request> request,
+                                std::shared_ptr<bt_interfaces_dummy::srv::TickCondition::Response>      response)
+{
+  std::lock_guard<std::mutex> lock(m_requestMutex);
+  RCLCPP_INFO(m_node->get_logger(), "HardwareFaultSkill::tick");
+  m_tickResult.store(Status::undefined);
+  m_stateMachine.submitEvent("CMD_TICK");
+  
+  while(m_tickResult.load()== Status::undefined) {
+      std::this_thread::sleep_for (std::chrono::milliseconds(100));
+  }
+  switch(m_tickResult.load()) 
+  {
+      
+      case Status::failure:
+          response->status = SKILL_FAILURE;
+          break;
+      case Status::success:
+          response->status = SKILL_SUCCESS;
+          break;
+      case Status::undefined:
+          response->status = SKILL_FAILURE;
+          break;
+  }
+  RCLCPP_INFO(m_node->get_logger(), "HardwareFaultSkill::tickDone");
+  response->is_ok = true;
+}
+
+
+
+
+
+
+
+
